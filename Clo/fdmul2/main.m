8e2f5e153b212adc4c25a4ea1531bb3d1899d290
Laurent Michel <ldm@engr.uconn.edu>
2012-07-02 15:04:43 +1000

    done

--- Clo/fdmul2/main.m
+++ Clo/fdmul2/main.m
@@ -42,17 +42,18 @@ int main(int argc, const char * argv[])
          [cp add:[CPFactory cardinality:x low:lb up:lb consistency:ValueConsistency]];
          
          id<CPExpr> lhs1 = [CPFactory dotProduct:(id<CPIntVar>[]){[x at:0],[x at:1],[x at:2],nil} by:(int[]){1,10,100}];
-         id<CPExpr> e1 = [CPFactory expr:lhs1 mul:[x at:3]];
-         id<CPExpr> e2 = [CPFactory expr:lhs1 mul:[x at:4]];
-         id<CPExpr> e3 = [CPFactory expr:lhs1 mul:[x at:5]];
          id<CPExpr> rhs1 = [CPFactory dotProduct:(id<CPIntVar>[]){[x at:6],[x at:7],[x at:8],nil} by:(int[]){1,10,100}];
          id<CPExpr> rhs2 = [CPFactory dotProduct:(id<CPIntVar>[]){[x at:9],[x at:10],[x at:11],nil} by:(int[]){1,10,100}];
          id<CPExpr> rhs3 = [CPFactory dotProduct:(id<CPIntVar>[]){[x at:12],[x at:13],[x at:14],nil} by:(int[]){1,10,100}];
-         [cp add:[CPFactory expr:[CPFactory expr:e1 equal:rhs1]]];
-         [cp add:[CPFactory expr:[CPFactory expr:e2 equal:rhs2]]];
-         [cp add:[CPFactory expr:[CPFactory expr:e3 equal:rhs3]]];
-         id<CPExpr> lhs4 = [CPFactory dotProduct:(id<CPIntVar>[]){[x at:15],[x at:16],[x at:17],[x at:18],[x at:19],nil} 
-                                              by:(int[]){1,10,100,1000,10000}];
+         [cp addRel:[[lhs1 mul:[x at:3]] equal:rhs1]];
+         [cp addRel:[[lhs1 mul:[x at:4]] equal:rhs2]];
+         [cp addRel:[[lhs1 mul:[x at:5]] equal:rhs3]];
+         id<CPExpr> lhs4 = [CPFactory sum:cp range:(CPRange){1,5} filteredBy:nil
+                                       of:^id<CPExpr>(CPInt i) {
+                                          return [[x at:14+i] muli: i * 10];
+                                       }];
+//         id<CPExpr> lhs4 = [CPFactory dotProduct:(id<CPIntVar>[]){[x at:15],[x at:16],[x at:17],[x at:18],[x at:19],nil} 
+//                                              by:(int[]){1,10,100,1000,10000}];
          id<CPExpr> rhs4 = [CPFactory dotProduct:(id<CPIntVar>[]){[x at:6],[x at:7],[x at:8],[x at:9],[x at:10],[x at:11],[x at:12],[x at:13],[x at:14],nil}
                                               by:(int[]){1,10,100,10,100,1000,100,1000,10000}];
          [cp add:[CPFactory expr:[CPFactory expr:lhs4 equal:rhs4]]];
