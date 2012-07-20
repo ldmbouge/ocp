7291d238035fb1fe1905f91fb2eceed28c2f9832
Pascal Van Hentenryck <pvh@nicta.com.au>
2012-07-20 14:02:03 +1000

    slab.dat

---
+++ Clo/slab/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  slab
+//
+//  Created by Pascal Van Hentenryck on 7/20/12.
+//  Copyright (c) 2012 CSE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+   @autoreleasepool {
+       
+       // insert code here...
+       NSLog(@"Hello, World!");
+       
+   }
+    return 0;
+}
+
