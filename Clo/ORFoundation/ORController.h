0cb10e58cbbc799e2e7b634a44764df8efe6072d
Pascal Van Hentenryck <pvh@nicta.com.au>
2012-08-03 22:01:51 -0700

    moving the searrch

---
+++ Clo/ORFoundation/ORController.h
@@ -0,0 +1,13 @@
+//
+//  ORController.h
+//  Clo
+//
+//  Created by Pascal Van Hentenryck on 8/3/12.
+//  Copyright (c) 2012 CSE. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ORController : NSView
+
+@end
---
+++ Clo/ORFoundation/ORController.m
@@ -0,0 +1,28 @@
+//
+//  ORController.m
+//  Clo
+//
+//  Created by Pascal Van Hentenryck on 8/3/12.
+//  Copyright (c) 2012 CSE. All rights reserved.
+//
+
+#import "ORController.h"
+
+@implementation ORController
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
