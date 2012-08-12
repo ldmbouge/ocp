0e5de0e26c9a529a8f2c61727933af31b4604af0
Pascal Van Hentenryck <pvh@nicta.com.au>
2012-08-12 20:14:34 +1000

    cleaning

--- Clo/objcp/CPArray.h
+++ Clo/objcp/CPArray.h
@@ -12,40 +12,6 @@
 #import "ORFoundation/ORArray.h"
 #import "objcp/CPData.h"
 
-/*
-@protocol CPIntArray <ORIntArray>
--(id<ORExpr>) elt: (id<ORExpr>) idx;
-@end
-
-
-@protocol CPVarArray <ORIdArray>
--(id<CPVar>) at: (ORInt) value;
--(void) set: (id<CPVar>) x at: (ORInt) value;
--(id<CPExpr>) elt: (id<CPExpr>) idx;
--(id<CPSolver>) cp;
-@end
-
-@protocol CPIntVarArray <CPVarArray> 
--(id<ORIntVar>) at: (ORInt) value;
--(void) set: (id<ORIntVar>) x at: (ORInt) value;
--(id<ORIntVar>)objectAtIndexedSubscript: (NSUInteger)key;
--(void)setObject: (id<ORIntVar>) newValue atIndexedSubscript: (NSUInteger) idx;
-@end
-
-@protocol CPIntVarMatrix <ORIdMatrix>
--(ORInt) arity;
--(id<ORIntVar>) flat:(ORInt)i;
--(id<ORIntVar>) at: (ORInt) i1 : (ORInt) i2;
--(id<ORIntVar>) at: (ORInt) i1 : (ORInt) i2 : (ORInt) i3;
--(void) set: (id) x at: (ORInt) i1 : (ORInt) i2;
--(void) set: (id) x at: (ORInt) i1 : (ORInt) i2 : (ORInt) i3;
--(id<ORIntRange>) range: (ORInt) i;
--(NSUInteger)count;
--(NSString*) description;
--(id<CPSolver>) cp;
-@end
-*/
-
 @protocol CPTRIntArray <NSObject> 
 -(ORInt)  at: (ORInt) value;
 -(void)  set: (ORInt) value at: (ORInt) value;  
