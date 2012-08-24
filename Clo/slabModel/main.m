2e23762b5a47a9f9e8c143e07a7f6f18c608f92e
Laurent Michel <ldm@engr.uconn.edu>
2012-08-24 10:35:14 -0400

    some fixes

--- Clo/slabModel/main.m
+++ Clo/slabModel/main.m
@@ -7,7 +7,9 @@
  License, v. 2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
  
- ***********************************************************************/#import <Foundation/Foundation.h>
+ ***********************************************************************/
+
+#import <Foundation/Foundation.h>
 #import <Foundation/Foundation.h>
 #import <ORUtilities/ORUtilities.h>
 #import <ORFoundation/ORSet.h>
