2caf585c19c20f4e64fa9b678466488ac71ecf1a
Laurent Michel <ldm@engr.uconn.edu>
2012-10-03 08:34:07 -0400

    coloring

--- Clo/coloringModel/main.m
+++ Clo/coloringModel/main.m
@@ -68,7 +68,7 @@ int main(int argc, const char * argv[])
       //id<CPSolver> cp = [CPFactory createSolver];
       //id<CPSemSolver> cp = [CPFactory createSemSolver:[ORSemDFSController class]];
       //id<CPSemSolver> cp = [CPFactory createSemSolver:[ORSemBDSController class]];
-      id<CPParSolver> cp = [CPFactory createParSolver:1 withController:[ORSemDFSController class]];
+      id<CPSolver> cp = [CPFactory createParSolver:2 withController:[ORSemDFSController class]];
       //id<CPParSolver> cp = [CPFactory createParSolver:2 withController:[ORSemBDSController class]];
       [cp addModel: model];
       
