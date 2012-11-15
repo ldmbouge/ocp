405b22dfa628081616f96bca83a123a9de6abfbe
Laurent Michel <ldm@engr.uconn.edu>
2012-11-15 15:20:22 -0500

    clean

---
+++ Clo/objcp/CPTypes.m
@@ -0,0 +1,20 @@
+/************************************************************************
+ Mozilla Public License
+ 
+ Copyright (c) 2012 NICTA, Laurent Michel and Pascal Van Hentenryck
+ 
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+ ***********************************************************************/
+
+#import <CPUKernel/CPUKernel.h>
+#import <CPUKernel/CPEngine.h>
+
+void failNow()
+{
+   static ORFailException* fex = nil;
+   if (fex==nil) fex = [ORFailException new];
+   @throw  CFRetain(fex);
+}
