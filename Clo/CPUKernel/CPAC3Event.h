251dcfb1bd2320561562a05e05112a9ed3bfdd0d
Laurent Michel <ldm@engr.uconn.edu>
2012-12-03 11:42:19 -0500

    added file

---
+++ Clo/CPUKernel/CPAC3Event.h
@@ -0,0 +1,26 @@
+/************************************************************************
+ Mozilla Public License
+ 
+ Copyright (c) 2012 NICTA, Laurent Michel and Pascal Van Hentenryck
+ 
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+ ***********************************************************************/
+
+#import <ORFoundation/ORFoundation.h>
+#import <CPUKernel/CPUKernel.h>
+
+@class CPCoreConstraint;
+
+@interface VarEventNode : NSObject<VarEventNode> {
+@public
+   VarEventNode*         _node;
+   id                 _trigger;  // type is {ConstraintCallback}
+   CPCoreConstraint*     _cstr;
+   ORInt             _priority;
+}
+-(VarEventNode*) initVarEventNode: (VarEventNode*) next trigger: (id) t cstr: (CPCoreConstraint*) c at: (ORInt) prio;
+-(void)dealloc;
+@end
