6f0e4317cda01f22eed990a8a9a3d5cda6e21523
Greg Johnson <gjohnson@engr.uconn.edu>
2012-12-21 22:49:51 -0500

    commit to pull down changes

--- Clo/MD5/MD4.h
+++ Clo/MD5/MD4.h
@@ -5,9 +5,30 @@
 //  Created by Greg Johnson on 12/18/12.
 //  Copyright (c) 2012 CSE. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
+#import <ORFoundation/ORModel.h>
+#import <ORModeling/ORModeling.h>
+#import <ORFoundation/ORFactory.h>
 
-@interface MD4 : NSObject
+@interface MD4 : NSObject{
+   @private
+   id<ORModel> _m;
+
+}
+-(void) initMD4;
+
+-(id<ORBitVar>) f:(id<ORBitVar>)x y:(id<ORBitVar>)y z:(id<ORBitVar>)z;
+-(id<ORBitVar>) g:(id<ORBitVar>)x y:(id<ORBitVar>)y z:(id<ORBitVar>)z;
+-(id<ORBitVar>) h:(id<ORBitVar>)x y:(id<ORBitVar>)y z:(id<ORBitVar>)z;
+-(id<ORBitVar>(*)[]) round1:(id<ORBitVar>[])h x:(id<ORBitVar>[]) x;
+-(id<ORBitVar>(*)[]) round2:(id<ORBitVar>[])h x:(id<ORBitVar>[]) x;
+-(id<ORBitVar>(*)[]) round3:(id<ORBitVar>[])h x:(id<ORBitVar>[]) x;
+-(id<ORBitVar>) shuffle1:(id<ORBitVar>)A b:(id<ORBitVar>)B c:(id<ORBitVar>)C d:(id<ORBitVar>) D index:(int)i shiftBy:(int) s x:(id<ORBitVar>[]) x;
+-(id<ORBitVar>) shuffle2:(id<ORBitVar>)A b:(id<ORBitVar>)B c:(id<ORBitVar>)C d:(id<ORBitVar>) D index:(int)i shiftBy:(int) s x:(id<ORBitVar>[]) x;
+-(id<ORBitVar>) shuffle3:(id<ORBitVar>)A b:(id<ORBitVar>)B c:(id<ORBitVar>)C d:(id<ORBitVar>) D index:(int)i shiftBy:(int) s x:(id<ORBitVar>[]) x;
+-(void) preimage:(int[]) message;
+
+
+//-(void) dealloc;
 
 @end
--- Clo/MD5/main.m
+++ Clo/MD5/main.m
@@ -6,17 +6,62 @@
 //  Copyright (c) 2012 CSE. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <ORFoundation/ORFoundation.h>
+#import <ORFoundation/ORAVLTree.h>
+#import <ORModeling/ORModeling.h>
+#import <ORProgram/ORProgram.h>
+#import <objcp/CPObjectQueue.h>
+#import <objcp/CPFactory.h>
+
+#import <objcp/CPConstraint.h>
+#import <objcp/CPBitMacros.h>
+#import <objcp/CPBitArray.h>
+#import <objcp/CPBitArrayDom.h>
+#import <objcp/CPBitConstraint.h>
 
 int main(int argc, const char * argv[])
 {
-
-   @autoreleasepool {
-       
-       // insert code here...
-       NSLog(@"Hello, World!");
-       
-   }
-    return 0;
+//
+//   @autoreleasepool {
+//      id<ORModel> m = [ORFactory createModel];
+//      unsigned int min;
+//      unsigned int max;
+//      min = min = 0;
+//      max = max = CP_UMASK;
+//      
+//      id<ORBitVar> x = [ORFactory bitVar:m low:&min up:&max bitLength:32];
+//      id<ORBitVar> y = [ORFactory bitVar:m low:&min up:&max bitLength:32];
+//      min[1] = 8;
+//      max[1] = 12;
+//      id<ORBitVar> z = [ORFactory bitVar:m low:&min up:&max bitLength:32];
+//      
+//      [m add:[ORFactory bit:x eq:y]];
+//      [m add:[ORFactory bit:y eq:z]];
+//
+//      id<CPProgram,CPBV> cp = [ORFactory createCPProgram:m];
+//      [cp solve: ^() {
+//         @try {
+//            NSLog(@"After Posting:");
+//            NSLog(@"a = %@\n", a);
+//            NSLog(@"b = %@\n", b);
+//            NSLog(@"c = %@\n", c);
+//            [cp labelUpFromLSB:a];
+//            [cp labelUpFromLSB:b];
+//            [cp labelUpFromLSB:c];
+//            NSLog(@"Solution Found:");
+//            NSLog(@"a = %@\n", a);
+//            NSLog(@"b = %@\n", b);
+//            NSLog(@"c = %@\n", c);
+//            
+//         }
+//         @catch (NSException *exception) {
+//            
+//            NSLog(@"MD5: Caught %@: %@", [exception name], [exception reason]);
+//            
+//         }
+//      }];
+//      
+//   }
+//    return 0;
 }
 
