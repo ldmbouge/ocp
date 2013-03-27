616dc0d829e07893df4ff4761581ffe11f6fcc08
Laurent Michel <ldm@engr.uconn.edu>
2013-03-27 09:07:27 -0400

    done

--- Clo/queensModel/main.m
+++ Clo/queensModel/main.m
@@ -10,48 +10,43 @@
  ***********************************************************************/
 
 #import <ORFoundation/ORFoundation.h>
-#import <ORFoundation/ORSemBDSController.h>
-#import <ORFoundation/ORSemDFSController.h>
-
 #import <ORModeling/ORModeling.h>
-#import <ORModeling/ORModelTransformation.h>
-#import <ORProgram/ORProgramFactory.h>
-
-
-// PVH Need to release the CPProgram
+#import <ORProgram/ORProgram.h>
 
 int main (int argc, const char * argv[])
 {
    ORInt n = 8;
-   id<ORModel> model = [ORFactory createModel];
-   
-   id<ORIntRange> R = RANGE(model,0,n-1);
-   id<ORInteger> nbSolutions = [ORFactory integer: model value: 0];
-   
-   id<ORIntVarArray> x  = [ORFactory intVarArray:model range:R domain: R];
-   id<ORIntVarArray> xp = [ORFactory intVarArray:model range:R with: ^id<ORIntVar>(ORInt i) { return [ORFactory intVar:model var:[x at: i] shift:i]; }];
-   id<ORIntVarArray> xn = [ORFactory intVarArray:model range:R with: ^id<ORIntVar>(ORInt i) { return [ORFactory intVar:model var:[x at: i] shift:-i]; }];
-   
-   [model add: [ORFactory alldifferent: x]];
-   [model add: [ORFactory alldifferent: xp]];
-   [model add: [ORFactory alldifferent: xn]];
-   
-   id<CPProgram> cp = [ORFactory createCPProgram: model];
-   [cp solveAll:
-    ^() {
-       [cp labelArray: x orderedBy: ^ORFloat(ORInt i) { return [x[i] domsize]; }];
-       for(int i = 0; i < n; i++)
-          printf("%d ",[x[i] value]);
-       printf("\n");
-       [nbSolutions incr];
-    }
-    ];
-   printf("GOT %d solutions\n",[nbSolutions value]);
-   NSLog(@"Solver status: %@\n",cp);
-   NSLog(@"Quitting");
-   NSLog(@"SOLUTION IS: %@",x);
-
-   NSLog(@"Done");
+   @autoreleasepool {
+      id<ORModel> model = [ORFactory createModel];      
+      id<ORIntRange> R = RANGE(model,0,n-1);
+      id<ORInteger> nbSolutions = [ORFactory integer: model value: 0];
+      
+      id<ORIntVarArray> x  = [ORFactory intVarArray:model range:R domain: R];
+      id<ORIntVarArray> xp = [ORFactory intVarArray:model range:R with: ^id<ORIntVar>(ORInt i) {
+         return [ORFactory intVar:model var:x[i] shift:i];
+      }];
+      id<ORIntVarArray> xn = [ORFactory intVarArray:model range:R with: ^id<ORIntVar>(ORInt i) {
+         return [ORFactory intVar:model var:x[i] shift:-i];
+      }];
+      
+      [model add: [ORFactory alldifferent: x]];
+      [model add: [ORFactory alldifferent: xp]];
+      [model add: [ORFactory alldifferent: xn]];
+      
+      id<CPProgram> cp = [ORFactory createCPProgram: model];
+      [cp solveAll:
+       ^() {
+          [cp labelArray: x orderedBy: ^ORFloat(ORInt i) { return x[i].domsize; }];
+          for(int i = 0; i < n; i++)
+             printf("%d ",x[i].value);
+          printf("\n");
+          [nbSolutions incr];
+       }
+       ];
+      NSLog(@"GOT %d solutions\n",nbSolutions.value);
+      NSLog(@"Solver status: %@\n",cp);
+      [ORFactory shutdown];
+   }
    return 0;
- }
+}
 
