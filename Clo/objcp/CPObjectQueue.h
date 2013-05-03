4b4b8de9320ee22eba7fd3bedf6cfd311b6ce4e3
Laurent Michel <ldm@engr.uconn.edu>
2013-05-03 13:56:07 -0400

    a few improvements

--- Clo/objcp/CPObjectQueue.h
+++ Clo/objcp/CPObjectQueue.h
@@ -32,14 +32,15 @@
 // Producer-Consumer queue
 @interface PCObjectQueue  : NSObject {
    ORInt           _mxs;
-   id*                 _tab;
+   id*             _tab;
    ORInt         _enter;
    ORInt          _exit;
    ORInt          _mask;  
    ORInt        _nbUsed;
    ORInt     _nbWorkers;
    ORInt    _nbWWaiting;
-   NSCondition*      _avail;
+   NSCondition*  _avail;
+   OSSpinLock    _slock;
 }
 -(id)initPCQueue:(ORInt)sz nbWorkers:(ORInt)nbw;
 -(void)dealloc;
