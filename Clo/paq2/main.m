f7445938f8762f51266ad856621df0aafe80ea3a
Laurent Michel <ldm@engr.uconn.edu>
2013-06-20 13:37:03 -0400

    paq2

--- Clo/paq2/main.m
+++ Clo/paq2/main.m
@@ -36,43 +36,39 @@ int main(int argc, const char * argv[])
          
          [model add:[Sum2(model, i, N, j, N, [[board at:i :j] eq:@1]) eq: nbQueens]];
          [model add:[Sum2(model, i, N, j, N, [[board at:i :j] eq:@2]) eq: nbQueens]];
-         
-         for(ORInt i=1;i<=n;i++) {
-            for(ORInt j=1;j<=n;j++) {
-               [model add:[[[board at:i :j] eq:@1] imply:
-                           [ORFactory and:model over:N
-                                 suchThat:nil
-                                       of:^id<ORRelation>(ORInt k) {
-                                          id<ORRelation> c = nil;
-                                          if (k != i) c = [[[board at:k :j] lt:@2] and:c];
-                                          if (k != j) c = [[[board at:i :k] lt:@2] and:c];
-                                          if (i+k <=n && j+k <= n) c = [[[board at:i+k :j+k] lt:@2] and:c];
-                                          if (i-k >0  && j-k > 0)  c = [[[board at:i-k :j-k] lt:@2] and:c];
-                                          if (i+k <=0 && j-k > 0)  c = [[[board at:i+k :j-k] lt:@2] and:c];
-                                          if (i-k > 0 && j+k <= n) c = [[[board at:i-k :j+k] lt:@2] and:c];
-                                          return c;
-                                       }]]];
-               [model add:[[[board at:i :j] eq:@2] imply:
-                           [ORFactory and:model over:N
-                                 suchThat:nil
-                                       of:^id<ORRelation>(ORInt k) {
-                                          id<ORRelation> c = nil;
-                                          if (k != i) c = [[[board at:k :j] neq:@1] and:c];
-                                          if (k != j) c = [[[board at:i :k] neq:@1] and:c];
-                                          if (i+k <=n && j+k <= n) c = [[[board at:i+k :j+k] neq:@1] and:c];
-                                          if (i-k >0  && j-k > 0)  c = [[[board at:i-k :j-k] neq:@1] and:c];
-                                          if (i+k <=0 && j-k > 0)  c = [[[board at:i+k :j-k] neq:@1] and:c];
-                                          if (i-k > 0 && j+k <= n) c = [[[board at:i-k :j+k] neq:@1] and:c];
-                                          return c;
-                                       }]]];
-            }
-         }
          [model maximize:nbQueens];
          id<CPProgram> cp = [args makeProgram:model];
          //id<CPHeuristic> h = [args makeHeuristic:cp restricted:nil];
          id<ORIntVarArray> x = All2(cp, ORIntVar, i, N, j, N, [board at:i :j]);
          __block ORInt nbSol = 0;
          [cp solve:^{
+            for(ORInt i=1;i<=n;i++) {
+               for(ORInt j=1;j<=n;j++) {
+                  [cp add:[[[board at:i :j] eq:@1] guard:^{
+                     for(ORInt k=1; k <= n;k++) {
+                        if (k != i) [cp add:[[board at:k :j] lt:@2]];
+                        if (k != j) [cp add:[[board at:i :k] lt:@2]];
+                        if (i+k <=n && j+k <= n) [cp add:[[board at:i+k :j+k] lt:@2]];
+                        if (i-k >0  && j-k > 0)  [cp add:[[board at:i-k :j-k] lt:@2]];
+                        if (i+k <=0 && j-k > 0)  [cp add:[[board at:i+k :j-k] lt:@2]];
+                        if (i-k > 0 && j+k <= n) [cp add:[[board at:i-k :j+k] lt:@2]];
+                     }
+                  }]];
+                  [cp add:[[[board at:i :j] eq:@2] imply:
+                              [ORFactory and:model over:N
+                                    suchThat:nil
+                                          of:^id<ORRelation>(ORInt k) {
+                                             id<ORRelation> c = nil;
+                                             if (k != i) c = [[[board at:k :j] neq:@1] and:c];
+                                             if (k != j) c = [[[board at:i :k] neq:@1] and:c];
+                                             if (i+k <=n && j+k <= n) c = [[[board at:i+k :j+k] neq:@1] and:c];
+                                             if (i-k >0  && j-k > 0)  c = [[[board at:i-k :j-k] neq:@1] and:c];
+                                             if (i+k <=0 && j-k > 0)  c = [[[board at:i+k :j-k] neq:@1] and:c];
+                                             if (i-k > 0 && j+k <= n) c = [[[board at:i-k :j+k] neq:@1] and:c];
+                                             return c;
+                                          }]]];
+               }
+            }
             for(ORInt k=0;k <= n*n-1;k++) {
                if ([cp bound:x[k]]) continue;
                while (![cp bound:x[k]]) {
