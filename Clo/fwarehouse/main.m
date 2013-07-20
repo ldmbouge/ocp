8b41e234fe6225a21020e30baf89fe3025eea75a
Laurent Michel <ldm@engr.uconn.edu>
2013-07-20 10:39:03 -0400

    done

--- Clo/fwarehouse/main.m
+++ Clo/fwarehouse/main.m
@@ -14,65 +14,72 @@
 #import <ORModeling/ORModeling.h>
 #import <ORProgram/ORProgramFactory.h>
 
+#import "ORCmdLineArgs.h"
+
 int main(int argc, const char * argv[])
 {
    @autoreleasepool {
-      id<ORModel> mdl = [ORFactory createModel];
-      ORInt fixed = 30;
-      ORFloat maxCost = 100;
-      id<ORIntRange> Stores     = RANGE(mdl,0,9);
-      id<ORIntRange> Warehouses = RANGE(mdl,0,4);
-      ORInt* cap = (ORInt[]){1,4,2,1,3};
-      ORFloat connection[10][5] = {{ 20, 24, 11, 25, 30 },
-         { 28, 27, 82, 83, 74 },
-         { 74, 97, 71, 96, 70 },
-         {  2, 55, 73, 69, 61 },
-         { 46, 96, 59, 83,  4 },
-         { 42, 22, 29, 67, 59 },
-         {  1,  5, 73, 59, 56 },
-         { 10, 73, 13, 43, 96 },
-         { 93, 35, 63, 85, 46 },
-         { 47, 65, 55, 71, 95 }};
-      ORFloat* conn = (ORFloat*)connection;
-      
-      
-      id<ORFloatVarArray> cost = [ORFactory floatVarArray: mdl range:Stores low:0 up:maxCost];
-      id<ORIntVarArray>   supp = [ORFactory intVarArray: mdl range:Stores domain: Warehouses];
-      id<ORIntVarArray>   open = [ORFactory intVarArray: mdl range:Warehouses domain: RANGE(mdl,0,1)];
-      id<ORFloatVar>      obj  = [ORFactory floatVar:mdl low:0 up:maxCost*Warehouses.size];
-      
-      [mdl add: [obj eq: [Sum(mdl,s, Stores, cost[s]) plus: Sum(mdl,w, Warehouses, [open[w] mul:@(fixed)]) ]]];
-      for(ORUInt i=Warehouses.low;i <= Warehouses.up;i++) {
-         [mdl add: [Sum(mdl,s, Stores, [supp[s] eq:@(i)]) leq:@(cap[i])]];
-      }
-      for(ORUInt i=Stores.low;i <= Stores.up; i++) {
-         id<ORFloatArray> row = [ORFactory floatArray:mdl range:Warehouses with:^ORFloat(ORInt j) { return conn[i*5+j];}];
-         [mdl add: [[open elt:supp[i]] eq:@1]];
-         [mdl add: [cost[i] eq:[row elt:supp[i]]]];
-      }
-      //[mdl minimize: [Sum(mdl,s, Stores, cost[s]) plus: Sum(mdl,w, Warehouses, [open[w] mul:@(fixed)])]];
-      [mdl minimize:obj];
-      
-      id<CPProgram> cp = [ORFactory createCPProgram:mdl];
-      __block ORInt nbSolutions = 0;
-      
-      [cp solve: ^{
-         NSLog(@"Start...");
-         //[cp labelArray:cost orderedBy:^ORFloat(ORInt i) { return [cp domsize:cost[i]];}];
-         [cp labelArray:supp orderedBy:^ORFloat(ORInt i) { return [cp domsize:supp[i]];}];
-         [cp labelArray:open orderedBy:^ORFloat(ORInt i) { return [cp domsize:open[i]];}];
-         nbSolutions++;
-         @autoreleasepool {
-            id<ORIntArray> ops = [ORFactory intArray:cp range:open.range with:^ORInt(ORInt k) {
-               return [cp intValue:open[k]];
-            }];
-            NSLog(@"Solution: %@  -- cost: %f",ops,[cp floatValue:obj]);
+      ORCmdLineArgs* args = [ORCmdLineArgs newWith:argc argv:argv];
+      [args measure:^struct ORResult(){
+         id<ORModel> mdl = [ORFactory createModel];
+         ORInt fixed = 30;
+         ORFloat maxCost = 100;
+         id<ORIntRange> Stores     = RANGE(mdl,0,9);
+         id<ORIntRange> Warehouses = RANGE(mdl,0,4);
+         ORInt* cap = (ORInt[]){1,4,2,1,3};
+         ORFloat connection[10][5] = {{ 20, 24, 11, 25, 30 },
+            { 28, 27, 82, 83, 74 },
+            { 74, 97, 71, 96, 70 },
+            {  2, 55, 73, 69, 61 },
+            { 46, 96, 59, 83,  4 },
+            { 42, 22, 29, 67, 59 },
+            {  1,  5, 73, 59, 56 },
+            { 10, 73, 13, 43, 96 },
+            { 93, 35, 63, 85, 46 },
+            { 47, 65, 55, 71, 95 }};
+         ORFloat* conn = (ORFloat*)connection;
+         
+         
+         id<ORFloatVarArray> cost = [ORFactory floatVarArray: mdl range:Stores low:0 up:maxCost];
+         id<ORIntVarArray>   supp = [ORFactory intVarArray: mdl range:Stores domain: Warehouses];
+         id<ORIntVarArray>   open = [ORFactory intVarArray: mdl range:Warehouses domain: RANGE(mdl,0,1)];
+         id<ORFloatVar>      obj  = [ORFactory floatVar:mdl low:0 up:maxCost*Warehouses.size];
+         
+         [mdl add: [obj eq: [Sum(mdl,s, Stores, cost[s]) plus: Sum(mdl,w, Warehouses, [open[w] mul:@(fixed)]) ]]];
+         for(ORUInt i=Warehouses.low;i <= Warehouses.up;i++) {
+            [mdl add: [Sum(mdl,s, Stores, [supp[s] eq:@(i)]) leq:@(cap[i])]];
          }
+         for(ORUInt i=Stores.low;i <= Stores.up; i++) {
+            id<ORFloatArray> row = [ORFactory floatArray:mdl range:Warehouses with:^ORFloat(ORInt j) { return conn[i*5+j];}];
+            [mdl add: [[open elt:supp[i]] eq:@1]];
+            [mdl add: [cost[i] eq:[row elt:supp[i]]]];
+         }
+         //[mdl minimize: [Sum(mdl,s, Stores, cost[s]) plus: Sum(mdl,w, Warehouses, [open[w] mul:@(fixed)])]];
+         [mdl minimize:obj];
+         
+         id<CPProgram> cp = [ORFactory createCPProgram:mdl];
+         __block ORInt nbSol = 0;
+         
+         [cp solve: ^{
+            NSLog(@"Start...");
+            //[cp labelArray:cost orderedBy:^ORFloat(ORInt i) { return [cp domsize:cost[i]];}];
+            [cp labelArray:supp orderedBy:^ORFloat(ORInt i) { return [cp domsize:supp[i]];}];
+            [cp labelArray:open orderedBy:^ORFloat(ORInt i) { return [cp domsize:open[i]];}];
+            nbSol++;
+            @autoreleasepool {
+               id<ORIntArray> ops = [ORFactory intArray:cp range:open.range with:^ORInt(ORInt k) {
+                  return [cp intValue:open[k]];
+               }];
+               NSLog(@"Solution: %@  -- cost: %f",ops,[cp floatValue:obj]);
+            }
+         }];
+         NSLog(@"#solutions: %d",nbSol);
+         NSLog(@"Solver: %@",cp);
+         struct ORResult res = REPORT(nbSol, [[cp explorer] nbFailures],[[cp explorer] nbChoices], [[cp engine] nbPropagation]);
+         [cp release];
+         [ORFactory shutdown];
+         return res;
       }];
-      NSLog(@"#solutions: %d",nbSolutions);
-      NSLog(@"Solver: %@",cp);
-      [cp release];
-      [ORFactory shutdown];
    }
    return 0;
 }
--- scripts/ounit.py
+++ scripts/ounit.py
@@ -89,7 +89,9 @@ class Runner:
 ab = [('queensAC',12,0),('ais',20,0),('fdmul',0,0),('costas',6,0),('golomb',8,0),
 	  ('bibd',8,0),('coloringModel',0,0),('debruijn',8,2),('eq20',0,0),('knapsack',4,0),
 	  ('knapsackOpt',3,0),('langford',8,2),('latinSquare',7,0),('magicserie',500,0),
-	  ('magicsquareModel',9,0),('perfect',0,0),('sport',0,0),('slab',0,0)]
+	  ('magicsquareModel',9,0),('perfect',0,0),('sport',0,0),('slab',0,0),('paq',6,0),
+	  ('qg7',9,0),('warehouse',0,0),('fwarehouse',0,0),('bacp',0,0),('nonogram',0,0),
+	  ('minesweeper',0,0),('circle',0,0)]
 for (b,qa,na) in ab:
 	p = Runner(b)
 	p.run(qa,na)
