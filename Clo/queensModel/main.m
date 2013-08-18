3ef6449310fb7a24bfdc51d56f940547c7ae04d0
Pascal Van Hentenryck <pvh@nicta.com.au>
2013-08-19 09:18:39 +1000

    Cleaner les failures: Part IIII

--- Clo/queensModel/main.m
+++ Clo/queensModel/main.m
@@ -12,14 +12,13 @@
 #import <ORFoundation/ORFoundation.h>
 #import <ORModeling/ORModeling.h>
 #import <ORProgram/ORProgram.h>
-#import <ORScheduler/ORScheduler.h>
 
 int main (int argc, const char * argv[])
 {
    ORInt n = 8;
    @autoreleasepool {
       id<ORModel> model = [ORFactory createModel];      
-      id<ORIntRange> R = RANGE(model,1,n);
+      id<ORIntRange> R = RANGE(model,0,n-1);
       id<ORMutableInteger> nbSolutions = [ORFactory mutable: model value: 0];
       
       id<ORIntVarArray> x  = [ORFactory intVarArray:model range:R domain: R];
@@ -34,13 +33,11 @@ int main (int argc, const char * argv[])
       [model add: [ORFactory alldifferent: xp]];
       [model add: [ORFactory alldifferent: xn]];
       
-      [model add: [ORFactory disjunctive: x[1] duration: 1 start: x[2] duration: 1]];
-      
       id<CPProgram> cp = [ORFactory createCPProgram: model];
-      [cp solve:
+      [cp solveAll:
        ^() {
           [cp labelArray: x orderedBy: ^ORFloat(ORInt i) { return [cp domsize:x[i]]; }];
-          for(int i = 1; i <= n; i++)
+          for(int i = 0; i < n; i++)
              printf("%d ",[cp intValue:x[i]]);
           printf("\n");
           [nbSolutions incr:cp];
