8d202d753e5e264cdc49548a3806c97be273d872
Pascal Van Hentenryck <pvh@nicta.com.au>
2013-10-27 19:08:02 -0700

    Relaxation work

--- Clo/LPRelaxation/main.m
+++ Clo/LPRelaxation/main.m
@@ -39,15 +39,16 @@ int main_lp(int argc, const char * argv[])
    id<ORIntRange> Columns = [ORFactory intRange: model low: 0 up: nbColumns-1];
    id<ORIntRange> Domain = [ORFactory intRange: model low: 0 up: 10000];
    id<ORIntVarArray> x = [ORFactory intVarArray: model range: Columns domain: Domain];
+   id<ORFloatVar> y = [ORFactory floatVar: model low: 0.0 up: 0.0];
    
    id<ORIdArray> ca = [ORFactory idArray:model range:RANGE(model,0,nbRows-1)];
    for(ORInt i = 0; i < nbRows; i++)
-      ca[i] = [model add: [Sum(model,j,Columns,[@(coef[i][j]) mul: x[j]]) leq: @(b[i])]];
+      ca[i] = [model add: [Sum(model,j,Columns,[@(coef[i][j]) mul: x[j]]) leq: [y plus: @(b[i])]]];
    [model maximize: Sum(model,j,Columns,[@(c[j]) mul: x[j]])];
    id<LPRelaxation> lp = [ORFactory createLPRelaxation: model];
    
    [lp solve];
-   
+   printf("Objective: %f \n",[lp objective]);
    for(ORInt i = 0; i < nbColumns-1; i++)
       printf("x[%d] = %10.5f : %10.5f \n",i,[lp floatValue: x[i]],[lp reducedCost: x[i]]);
    for(ORInt i = 0; i < nbRows; i++)
@@ -67,6 +68,59 @@ int main_lp(int argc, const char * argv[])
    return 0;
 }
 
+int main_cp(int argc, const char * argv[])
+{
+   @autoreleasepool {
+      ORCmdLineArgs* args = [ORCmdLineArgs newWith:argc argv:argv];
+      [args measure:^struct ORResult() {
+         ORLong startTime = [ORRuntimeMonitor cputime];
+         id<ORModel> model = [ORFactory createModel];
+         id<ORIntRange> Columns = [ORFactory intRange: model low: 0 up: nbColumns-1];
+         id<ORIntRange> Domain = [ORFactory intRange: model low: 0 up: 10000];
+         id<ORIntVarArray> x = [ORFactory intVarArray: model range: Columns domain: Domain];
+         id<ORFloatVar> y = [ORFactory floatVar: model low: 0.0 up: 0.0];
+         
+         //   id<ORIdArray> ca = [ORFactory idArray:model range:RANGE(model,0,nbRows-1)];
+         for(ORInt i = 0; i < nbRows; i++)
+            [model add: [Sum(model,j,Columns,[@(coef[i][j]) mul: x[j]]) leq: [y plus: @(b[i])]]];
+         [model maximize: Sum(model,j,Columns,[@(c[j]) mul: x[j]])];
+         
+         
+         id<CPProgram> cp = [ORFactory createCPProgram: model];
+         [cp solve:
+          ^() {
+             for(ORInt i = 0; i < nbColumns; i++) {
+                NSLog(@"Variable x[%d]=[%d,%d]",i,[cp min: x[i]],[cp max: x[i]]);
+             }
+             for(ORInt i = 0; i < nbColumns; i++) {
+                //          NSLog(@"Variable x[%d]=[%d,%d]",i,[cp min: x[i]],[cp max: x[i]]);
+                while (![cp bound: x[i]]) {
+                   ORInt m = ([cp max: x[i]] + [cp min: x[i]]) / 2;
+                   //             NSLog(@"Mid value: %d for [%d,%d]",m,[cp min: x[i]],[cp max: x[i]]);
+                   [cp try:
+                    ^()  { [cp gthen: x[i] with: m]; /* NSLog(@"After gthen %d: [%d,%d]",i,[cp min: x[i]],[cp max: x[i]]); */}
+                        or:
+                    ^()  { [cp lthen: x[i] with: m+1]; /* NSLog(@"After lthen %d: [%d,%d]",i,[cp min: x[i]],[cp max: x[i]]); */}
+                    
+                    ];
+                }
+             }
+             //      for(ORInt i = 0; i < nbColumns; i++)
+             //         NSLog(@"Value of x[%d] = %d",i,[cp intValue: x[i]]);
+          }
+          ];
+         ORLong endTime = [ORRuntimeMonitor cputime];
+         printf("Execution Time: %lld \n",endTime - startTime);
+         NSLog(@"we are done \n\n");
+         ORInt valueSol = [(id<ORObjectiveValueInt>)[[[cp solutionPool] best] objectiveValue] value];
+         struct ORResult r = REPORT(valueSol, [[cp explorer] nbFailures],[[cp explorer] nbChoices], [[cp engine] nbPropagation]);
+         [cp release];
+         return r;
+      }];
+   }
+   return 0;
+}
+
 int main_hybrid(int argc, const char * argv[])
 {
    @autoreleasepool {
@@ -77,10 +131,11 @@ int main_hybrid(int argc, const char * argv[])
          id<ORIntRange> Columns = [ORFactory intRange: model low: 0 up: nbColumns-1];
          id<ORIntRange> Domain = [ORFactory intRange: model low: 0 up: 10000];
          id<ORIntVarArray> x = [ORFactory intVarArray: model range: Columns domain: Domain];
+         id<ORFloatVar> y = [ORFactory floatVar: model low: 0.0 up: 0.0];
          
          //   id<ORIdArray> ca = [ORFactory idArray:model range:RANGE(model,0,nbRows-1)];
          for(ORInt i = 0; i < nbRows; i++)
-            [model add: [Sum(model,j,Columns,[@(coef[i][j]) mul: x[j]]) leq: @(b[i])]];
+               [model add: [Sum(model,j,Columns,[@(coef[i][j]) mul: x[j]]) leq: [y plus: @(b[i])]]];
          [model maximize: Sum(model,j,Columns,[@(c[j]) mul: x[j]])];
          
          id<ORRelaxation> lp = [ORFactory createLinearRelaxation: model];
@@ -129,5 +184,6 @@ int main_hybrid(int argc, const char * argv[])
 int main(int argc, const char * argv[])
 {
 //   main_lp(argc,argv);
-   return main_hybrid(argc,argv);
+//   return main_hybrid(argc,argv);
+   return main_cp(argc,argv);
 }
