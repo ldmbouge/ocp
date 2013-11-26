310c5ac7b238d1cfe1b47934893c46c407284ed8
Greg Johnson <gjohnson@engr.uconn.edu>
2013-11-26 15:41:50 -0500

    added sha1b class files

---
+++ Clo/SHA1/SHA1/SHA1b.h
@@ -0,0 +1,13 @@
+//
+//  SHA1b.h
+//  Clo
+//
+//  Created by Greg Johnson on 11/7/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHA1b : NSObject
+
+@end
---
+++ Clo/SHA1/SHA1/SHA1b.m
@@ -0,0 +1,13 @@
+//
+//  SHA1b.m
+//  Clo
+//
+//  Created by Greg Johnson on 11/7/13.
+//
+//
+
+#import "SHA1b.h"
+
+@implementation SHA1b
+
+@end
