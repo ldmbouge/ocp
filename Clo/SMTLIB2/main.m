d182840118b28e0c517ea3361f2449f46b1fac9a
Greg Johnson <gjohnson@engr.uconn.edu>
2014-02-24 14:20:14 -0500

    initial commit of verification testing

---
+++ Clo/SMTLIB2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SMTLIB2
+//
+//  Created by Greg Johnson on 2/24/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+   @autoreleasepool {
+       
+       // insert code here...
+       NSLog(@"Hello, World!");
+       
+   }
+    return 0;
+}
+
