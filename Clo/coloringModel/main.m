ef0a3aa0232cc178398efb4cc4cfb5a3f05a7893
dan <daniel.fontaine@gmail.com>
2014-05-07 07:21:02 -0400

    update

--- Clo/coloringModel/main.m
+++ Clo/coloringModel/main.m
@@ -41,7 +41,7 @@ int main(int argc, const char * argv[])
       ORFloat timeLimit = 5 * 60;
       
       id<ORModel> model = [ORFactory createModel];
-      FILE* dta = fopen("/Users/dan/Desktop/clique.col","r");  // file is located in the executable directory.
+      FILE* dta = fopen("/Users/dan/Desktop/LRPaperStuff/clique.col","r");  // file is located in the executable directory.
       //FILE* dta = fopen("smallColoring.col","r");
       //FILE* dta = fopen("test-n30-e50.col","r");
       //FILE* dta = fopen("test-n80-p40-0.col","r");
@@ -81,7 +81,14 @@ int main(int argc, const char * argv[])
       [model minimize: m];
       free(edges);
       
-      
+      ORInt (^xdeg)(id<ORIntVar>) = ^ORInt(id<ORIntVar> x) {
+         int d = 0;
+         for(id<ORConstraint> c in [model constraints]) {
+            if([[c allVars] containsObject: x]) d++;
+         }
+         return d;
+      };
+   
       // FIND RELAXATION -------------------------------------------------------------------------------
       ORFloat UB = 43;
       NSArray* split = [ORSubgradientTemplate autosplitVariables: [c toNSArray] constraints: nonCoupledCstr];
@@ -90,7 +97,31 @@ int main(int argc, const char * argv[])
          NSArray* allObjs = [(NSSet*)obj allObjects];
          [varSets addObject: [ORFactory idArray: model NSArray: allObjs]];
       }];
+   
+   // DEBUGGING
+   NSMutableArray* idSplit = [[NSMutableArray alloc] init];
+   NSMapTable* idDeg = [[NSMapTable alloc] init];
+   for(NSSet* s in split) {
+      NSMutableArray* arr2 = [s allObjects];
+      [idSplit addObject: arr2];
       
+      for(id<ORIntVar> x in s) {
+         [idDeg setObject: @(xdeg(x)) forKey: x];
+      }
+   }
+   
+   char label = 'A';
+   for(NSArray* arr in idSplit) {
+      NSLog(@"clique %c(size = %li)", label++, arr.count);
+      ORInt totalDeg = 0;
+      for(id<ORIntVar> x in arr) {
+         ORInt d = [[idDeg objectForKey: x] intValue];
+         NSLog(@"c[%i] (deg=%i)", [x getId]-1, d);
+         totalDeg += d;
+      }
+      NSLog(@"degree of clique %i -------------\n\n", totalDeg);
+   }
+   
       NSMutableArray* maxCliques = [[NSMutableArray alloc] initWithCapacity: 8];
       __block ORInt maxCliqueSize = 0;
       [split enumerateObjectsUsingBlock: ^(id obj, NSUInteger idx, BOOL* stop) {
@@ -129,163 +160,163 @@ int main(int argc, const char * argv[])
       fprintf(f, "\n%i-%i-%i--------------------\n", nbv, cliqueCount, relaxCount);
    
    
-      // LR-MIP -------------------------------------------
-      ORLagrangianTransform* t = [ORFactory lagrangianViolationTransform];
-      id<ORParameterizedModel> lagrangeModel0 = [t apply: lm relaxing: relaxCstrs];
-      id<ORParameterizedModel> lagrangeModel1 = [t apply: lm relaxing: unrelaxCstrs];
-
-      id<ORRunnable> r0 = [ORFactory MIPSubgradient: lagrangeModel0 bound: UB];
-      [(ORSubgradientTemplate*)r0 setAgility: 3];
-      id<ORRunnable> r1 = [ORFactory MIPSubgradient: lagrangeModel1 bound: UB];
-      [(ORSubgradientTemplate*)r1 setAgility: 3];
-      //id<ORRunnable> pr = [ORFactory composeCompleteParallel: r0 with: r1];
-      [r0 setTimeLimit: timeLimit];//[pr setTimeLimit: timeLimit];
-      //[(MIPSubgradient*)r setSolverTimeLimit: 5];
-      [r0 run];
-      NSTimeInterval time = [(ORSubgradientTemplate*)r0 runtime];
-      if(time > timeLimit) time = timeLimit;
-      ORFloat bnd = [r0 bestBound];//[pr bestBound];
-      if(fabs(bnd) > 1000) bnd = -1;
-      id<ORSolution> sol = [r0 bestSolution];//[pr bestSolution];
-      ORFloat inc = sol ? [[sol objectiveValue] floatValue] : -1;
-      if(fabs(inc) > 1000) inc = -1;
-      ORInt iter = [r0 iterations];//MAX([r0 iterations], [r1 iterations]);
-      //id<ORRunnable> solved = [pr solvedRunnable];
-      //if(solved) iter = [solved iterations];
-      [t release];
-      [r0 release];
-      [r1 release];
-      //[pr release];
-      [lagrangeModel0 release];
-      //[lagrangeModel1 release];
-      fprintf(f, "LR: %f %f %f %i\n", time, bnd, inc, iter);
-      fflush(f);
-   
-      // LR-MIP Violation -----------------------------------------
-
-      t = [ORFactory lagrangianViolationTransform];
-      lagrangeModel0 = [t apply: lm relaxing: relaxCstrs];
-      //lagrangeModel1 = [t apply: lm relaxing: unrelaxCstrs];
-      r0 = [ORFactory MIPSubgradient: lagrangeModel0 bound: UB];
-      //r1 = [ORFactory MIPSubgradient: lagrangeModel1 bound: UB];
-      //pr = [ORFactory composeCompleteParallel: r0 with: r1];
-      [r0 setTimeLimit: timeLimit];//[pr setTimeLimit: timeLimit];
-      //[(MIPSubgradient*)r setSolverTimeLimit: 5];
-      [r0 run];
-      time = [(ORSubgradientTemplate*)r0 runtime];
-      if(time > timeLimit) time = timeLimit;
-      bnd = [r0 bestBound];//[pr bestBound];
-      if(fabs(bnd) > 1000) bnd = -1;
-      sol = [r0 bestSolution];//[pr bestSolution];
-      inc = sol ? [[sol objectiveValue] floatValue] : -1;
-      if(fabs(inc) > 1000) inc = -1;
-      iter = [r0 iterations];//MAX([r0 iterations], [r1 iterations]);
-      //solved = [pr solvedRunnable];
-      //if(solved) iter = [solved iterations];
-      [t release];
-      [r0 release];
-      //[r1 release];
-      //[pr release];
-      [lagrangeModel0 release];
-      //[lagrangeModel1 release];
-      fprintf(f, "LRV: %f %f %f %i\n", time, bnd, inc, iter);
-      fflush(f);
-   
-      // Full MIP ------------------------------------------
+//      // LR-MIP -------------------------------------------
+//      ORLagrangianTransform* t = [ORFactory lagrangianViolationTransform];
+//      id<ORParameterizedModel> lagrangeModel0 = [t apply: lm relaxing: relaxCstrs];
+//      id<ORParameterizedModel> lagrangeModel1 = [t apply: lm relaxing: unrelaxCstrs];
+//
+//      id<ORRunnable> r0 = [ORFactory MIPSubgradient: lagrangeModel0 bound: UB];
+//      [(ORSubgradientTemplate*)r0 setAgility: 3];
+//      id<ORRunnable> r1 = [ORFactory MIPSubgradient: lagrangeModel1 bound: UB];
+//      [(ORSubgradientTemplate*)r1 setAgility: 3];
+//      //id<ORRunnable> pr = [ORFactory composeCompleteParallel: r0 with: r1];
+//      [r0 setTimeLimit: timeLimit];//[pr setTimeLimit: timeLimit];
+//      //[(MIPSubgradient*)r setSolverTimeLimit: 5];
+//      [r0 run];
+//      NSTimeInterval time = [(ORSubgradientTemplate*)r0 runtime];
+//      if(time > timeLimit) time = timeLimit;
+//      ORFloat bnd = [r0 bestBound];//[pr bestBound];
+//      if(fabs(bnd) > 1000) bnd = -1;
+//      id<ORSolution> sol = [r0 bestSolution];//[pr bestSolution];
+//      ORFloat inc = sol ? [[sol objectiveValue] floatValue] : -1;
+//      if(fabs(inc) > 1000) inc = -1;
+//      ORInt iter = [r0 iterations];//MAX([r0 iterations], [r1 iterations]);
+//      //id<ORRunnable> solved = [pr solvedRunnable];
+//      //if(solved) iter = [solved iterations];
+//      [t release];
+//      [r0 release];
+//      [r1 release];
+//      //[pr release];
+//      [lagrangeModel0 release];
+//      //[lagrangeModel1 release];
+//      fprintf(f, "LR: %f %f %f %i\n", time, bnd, inc, iter);
+//      fflush(f);
+//   
+//      // LR-MIP Violation -----------------------------------------
+//
+//      t = [ORFactory lagrangianViolationTransform];
+//      lagrangeModel0 = [t apply: lm relaxing: relaxCstrs];
+//      //lagrangeModel1 = [t apply: lm relaxing: unrelaxCstrs];
+//      r0 = [ORFactory MIPSubgradient: lagrangeModel0 bound: UB];
+//      //r1 = [ORFactory MIPSubgradient: lagrangeModel1 bound: UB];
+//      //pr = [ORFactory composeCompleteParallel: r0 with: r1];
+//      [r0 setTimeLimit: timeLimit];//[pr setTimeLimit: timeLimit];
+//      //[(MIPSubgradient*)r setSolverTimeLimit: 5];
+//      [r0 run];
+//      time = [(ORSubgradientTemplate*)r0 runtime];
+//      if(time > timeLimit) time = timeLimit;
+//      bnd = [r0 bestBound];//[pr bestBound];
+//      if(fabs(bnd) > 1000) bnd = -1;
+//      sol = [r0 bestSolution];//[pr bestSolution];
+//      inc = sol ? [[sol objectiveValue] floatValue] : -1;
+//      if(fabs(inc) > 1000) inc = -1;
+//      iter = [r0 iterations];//MAX([r0 iterations], [r1 iterations]);
+//      //solved = [pr solvedRunnable];
+//      //if(solved) iter = [solved iterations];
+//      [t release];
+//      [r0 release];
+//      //[r1 release];
+//      //[pr release];
+//      [lagrangeModel0 release];
+//      //[lagrangeModel1 release];
+//      fprintf(f, "LRV: %f %f %f %i\n", time, bnd, inc, iter);
+//      fflush(f);
+//   
+//      // Full MIP ------------------------------------------
       id<ORRunnable> r = [ORFactory MIPRunnable: lm];
       [r setTimeLimit: timeLimit];
       //[(MIPSubgradient*)r setSolverTimeLimit: 5];
       NSDate* t0 = [NSDate date];
       [r run];
       NSDate* t1 = [NSDate date];
-      time = [t1 timeIntervalSinceDate: t0];
+      NSTimeInterval time = [t1 timeIntervalSinceDate: t0];
       if(time > timeLimit) time = timeLimit;
-      bnd = [(id<ORLagrangeRelax>)r bestBound];
+      ORInt bnd = [(id<ORLagrangeRelax>)r bestBound];
       if(fabs(bnd) > 1000) bnd = -1;
-      sol = [(id<ORLagrangeRelax>)r bestSolution];
-      inc = sol ? [[sol objectiveValue] floatValue] : -1;
+      id<ORSolution> sol = [(id<ORLagrangeRelax>)r bestSolution];
+      ORFloat inc = sol ? [[sol objectiveValue] floatValue] : -1;
       if(fabs(inc) > 1000) inc = -1;
       [r release];
       fprintf(f, "MIP: %f %f %f\n", time, bnd, inc);
       fflush(f);
-      
-      
-//      // TEST CP-LR -----------------------------------------------------------------------
-      NSSortDescriptor *sd = [NSSortDescriptor sortDescriptorWithKey:@"count" ascending:NO];
-      NSArray *sds = [NSArray arrayWithObject:sd];
-      NSArray* searchSets = [varSets sortedArrayUsingDescriptors: sds];
-      
-      ORLagrangianTransform* t4 = [ORFactory lagrangianViolationTransform];
-      lagrangeModel0 = [t4 apply: model relaxing: relaxCstrs];
-      lagrangeModel1 = [t4 apply: model relaxing: unrelaxCstrs];
-      id<ORIntVarArray> slacks0 = (id<ORIntVarArray>)[lagrangeModel0 slacks];
-      id<ORIntVarArray> slacks1 = (id<ORIntVarArray>)[lagrangeModel1 slacks];
-      
-      void (^search0)(id<CPCommonProgram>) = ^(id<CPCommonProgram> cp){
-         for(id<ORIntVarArray> vars in searchSets) {
-            //NSLog(@"**CLIQUE: %@",vars);
-            
-            [vars enumerateWith: ^(id obj, int idx) {
-               ORInt maxc = max(0,[cp maxBound: c]);
-               //NSLog(@"VARIABLE:%d -  %@ ",idx,obj);
-               [cp tryall: V
-                 suchThat:^bool(ORInt v) { return v <= maxc+1 && [cp member: v in: obj];}
-                       in:^(ORInt v) { [cp label: obj with: v]; }
-                onFailure:^(ORInt v) {
-                   [cp diff: obj with:v];
-                }
-                ];
-            }];
-         }
-         [cp label:m with:[cp min: m]];
-         [cp labelArray: slacks0];
-      };
-      
-      void (^search1)(id<CPCommonProgram>) = ^(id<CPCommonProgram> cp){
-         for(id<ORIntVarArray> vars in searchSets) {
-            //NSLog(@"**CLIQUE: %@",vars);
-            
-            [vars enumerateWith: ^(id obj, int idx) {
-               ORInt maxc = max(0,[cp maxBound: c]);
-               //NSLog(@"VARIABLE:%d -  %@ ",idx,obj);
-               [cp tryall: V
-                 suchThat:^bool(ORInt v) { return v <= maxc+1 && [cp member: v in: obj];}
-                       in:^(ORInt v) { [cp label: obj with: v]; }
-                onFailure:^(ORInt v) {
-                   [cp diff: obj with:v];
-                }
-                ];
-            }];
-         }
-         [cp label:m with:[cp min: m]];
-         [cp labelArray: slacks1];
-      };
-      
-      r0 = [ORFactory CPSubgradient: lagrangeModel0 bound: UB search: search0];
-      [(ORSubgradientTemplate*)r0 setAgility: 30];
-      r1 = [ORFactory CPSubgradient: lagrangeModel1 bound: UB search: search1];
-      [(ORSubgradientTemplate*)r1 setAgility: 30];
-      id<ORRunnable> pr = [ORFactory composeCompleteParallel: r0 with: r1];
-      [pr setTimeLimit: timeLimit];
-
-      t0 = [NSDate date];
-      [pr run];
-      t1 = [NSDate date];
-      time = [t1 timeIntervalSinceDate: t0];
-      ORFloat bnd4 = [pr bestBound];
-      iter = MAX([(ORSubgradientTemplate*)r0 iterations], [(ORSubgradientTemplate*)r1 iterations]);
-      id<ORRunnable> solved = [pr solvedRunnable];
-      if(solved) iter = [solved iterations];
-      if(fabs(bnd4) > 1000) bnd4 = -1;
-      fprintf(f, "CPLR: %f %f %f %i\n", time, bnd4, -1.0, iter);
-      fflush(f);
-      [lagrangeModel0 release];
-      [lagrangeModel1 release];
-      [r0 release];
-      [r1 release];
-      [pr release];
-      [t4 release];
-      
+//
+//      
+////      // TEST CP-LR -----------------------------------------------------------------------
+//      NSSortDescriptor *sd = [NSSortDescriptor sortDescriptorWithKey:@"count" ascending:NO];
+//      NSArray *sds = [NSArray arrayWithObject:sd];
+//      NSArray* searchSets = [varSets sortedArrayUsingDescriptors: sds];
+//      
+//      ORLagrangianTransform* t4 = [ORFactory lagrangianViolationTransform];
+//      lagrangeModel0 = [t4 apply: model relaxing: relaxCstrs];
+//      lagrangeModel1 = [t4 apply: model relaxing: unrelaxCstrs];
+//      id<ORIntVarArray> slacks0 = (id<ORIntVarArray>)[lagrangeModel0 slacks];
+//      id<ORIntVarArray> slacks1 = (id<ORIntVarArray>)[lagrangeModel1 slacks];
+//      
+//      void (^search0)(id<CPCommonProgram>) = ^(id<CPCommonProgram> cp){
+//         for(id<ORIntVarArray> vars in searchSets) {
+//            //NSLog(@"**CLIQUE: %@",vars);
+//            
+//            [vars enumerateWith: ^(id obj, int idx) {
+//               ORInt maxc = max(0,[cp maxBound: c]);
+//               //NSLog(@"VARIABLE:%d -  %@ ",idx,obj);
+//               [cp tryall: V
+//                 suchThat:^bool(ORInt v) { return v <= maxc+1 && [cp member: v in: obj];}
+//                       in:^(ORInt v) { [cp label: obj with: v]; }
+//                onFailure:^(ORInt v) {
+//                   [cp diff: obj with:v];
+//                }
+//                ];
+//            }];
+//         }
+//         [cp label:m with:[cp min: m]];
+//         [cp labelArray: slacks0];
+//      };
+//      
+//      void (^search1)(id<CPCommonProgram>) = ^(id<CPCommonProgram> cp){
+//         for(id<ORIntVarArray> vars in searchSets) {
+//            //NSLog(@"**CLIQUE: %@",vars);
+//            
+//            [vars enumerateWith: ^(id obj, int idx) {
+//               ORInt maxc = max(0,[cp maxBound: c]);
+//               //NSLog(@"VARIABLE:%d -  %@ ",idx,obj);
+//               [cp tryall: V
+//                 suchThat:^bool(ORInt v) { return v <= maxc+1 && [cp member: v in: obj];}
+//                       in:^(ORInt v) { [cp label: obj with: v]; }
+//                onFailure:^(ORInt v) {
+//                   [cp diff: obj with:v];
+//                }
+//                ];
+//            }];
+//         }
+//         [cp label:m with:[cp min: m]];
+//         [cp labelArray: slacks1];
+//      };
+//      
+//      r0 = [ORFactory CPSubgradient: lagrangeModel0 bound: UB search: search0];
+//      [(ORSubgradientTemplate*)r0 setAgility: 30];
+//      r1 = [ORFactory CPSubgradient: lagrangeModel1 bound: UB search: search1];
+//      [(ORSubgradientTemplate*)r1 setAgility: 30];
+//      id<ORRunnable> pr = [ORFactory composeCompleteParallel: r0 with: r1];
+//      [pr setTimeLimit: timeLimit];
+//
+//      t0 = [NSDate date];
+//      [pr run];
+//      t1 = [NSDate date];
+//      time = [t1 timeIntervalSinceDate: t0];
+//      ORFloat bnd4 = [pr bestBound];
+//      iter = MAX([(ORSubgradientTemplate*)r0 iterations], [(ORSubgradientTemplate*)r1 iterations]);
+//      id<ORRunnable> solved = [pr solvedRunnable];
+//      if(solved) iter = [solved iterations];
+//      if(fabs(bnd4) > 1000) bnd4 = -1;
+//      fprintf(f, "CPLR: %f %f %f %i\n", time, bnd4, -1.0, iter);
+//      fflush(f);
+//      [lagrangeModel0 release];
+//      [lagrangeModel1 release];
+//      [r0 release];
+//      [r1 release];
+//      [pr release];
+//      [t4 release];
+   
       // TEST FULL CP -----------------------------------------------------------------
       __block cpbnd = 99999;
       __block ORFloat bndtime = -1;
@@ -313,7 +344,7 @@ int main(int argc, const char * argv[])
             NSDate* t2 = [NSDate date];
             NSTimeInterval time2 = [t2 timeIntervalSinceDate: t0];
             if([cp intValue:m] < cpbnd) { cpbnd = [cp intValue:m]; bndtime = time2; }
-            //NSLog(@"coloring with: %d colors -- time %f",[cp intValue:m], time2);
+            NSLog(@"coloring with: %d colors -- time %f",[cp intValue:m], time2);
          }];
       }];
       t1 = [NSDate date];
