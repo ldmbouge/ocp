468b68cdc3002028b6ee63b65cb1d6249b309ced
Laurent Michel <ldm@engr.uconn.edu>
2015-07-04 12:48:36 -0400

    refactored the void array creation

--- Clo/CPSwift.playground/Contents.swift
+++ Clo/CPSwift.playground/Contents.swift
@@ -3,14 +3,94 @@
 import ORFoundation
 import ORProgram
 
-func !=(lhs : ORExpr,rhs : ORExpr) -> ORRelation {
+public func ==(lhs : ORExpr,rhs : ORExpr) -> ORRelation {
+   return lhs.eq(rhs)
+}
+public func ==(lhs : ORExpr,rhs : Int) -> ORRelation {
+   return lhs.eq(ORFactory.integer(lhs.tracker(), value: ORInt(rhs)))
+}
+public func ==(lhs : ORExpr,rhs : Int32) -> ORRelation {
+   return lhs.eq(ORFactory.integer(lhs.tracker(), value: ORInt(rhs)))
+}
+public func ==(lhs : ORExpr,rhs : ORIntVar!) -> ORRelation {
+   return lhs.eq(rhs)
+}
+public func !=(lhs : ORExpr,rhs : ORExpr) -> ORRelation {
    return lhs.neq(rhs)
 }
-func +(lhs: ORExpr,rhs : Int) -> ORExpr {
+infix operator ≠ { associativity left precedence 130 }
+public func ≠(lhs : ORExpr,rhs : ORExpr) -> ORRelation {
+   return lhs.neq(rhs)
+}
+public func +(lhs: ORExpr,rhs : Int) -> ORExpr {
    return lhs.plus(rhs);
 }
-func +(lhs: ORExpr,rhs : AnyObject) -> ORExpr {
+public func +(lhs: ORExpr,rhs : AnyObject) -> ORExpr {
    return lhs.plus(rhs);
+}
+public func *(lhs: ORExpr, rhs : Int) -> ORExpr {
+   return lhs.mul(rhs)
+}
+public func *(lhs: ORExpr, rhs : ORInt) -> ORExpr {
+   return lhs.mul(ORFactory.integer(lhs.tracker(), value: ORInt(rhs)))
+}
+
+func convertArray(s : [UnsafeMutablePointer<Void>]) -> [AnyObject] {
+   return s.map({v  in Unmanaged<AnyObject>.fromOpaque(COpaquePointer(v)).takeUnretainedValue() })
+}
+
+func wrap<T>(x : T) -> UnsafeMutablePointer<Void> {
+   return unsafeBitCast(x, UnsafeMutablePointer<Void>.self)
+}
+func unwrap<T>(x : UnsafeMutablePointer<Void>) -> T {
+   return unsafeBitCast(x, T.self)
+}
+
+typealias VoidPtr = UnsafeMutablePointer<Void>
+typealias VoidBuf = UnsafeMutableBufferPointer<VoidPtr>
+
+infix operator » { associativity left precedence 70 }
+infix operator | { associativity left precedence 80 }
+
+func getSolver(a : VoidPtr) -> CPCommonProgram
+{
+   var at : AnyObject = Unmanaged<AnyObject>.fromOpaque(COpaquePointer(a)).takeUnretainedValue()
+   var tracker =  at.tracker() as! CPCommonProgram
+   return tracker
+}
+
+func packageVoidArray(sz : Int,body : (Int32,UnsafeMutablePointer<VoidPtr>,VoidBuf) -> VoidPtr) -> VoidPtr
+{
+   var ptr = UnsafeMutablePointer<VoidPtr>.alloc(sz)
+   var ta = UnsafeMutableBufferPointer<VoidPtr>(start: ptr, count: sz)
+   let rv = body(Int32(sz), ptr,ta)
+   ptr.dealloc(sz)
+   return rv;
+}
+
+public func »(a : UnsafeMutablePointer<Void>, b : UnsafeMutablePointer<Void>) -> UnsafeMutablePointer<Void> {
+   return packageVoidArray(2) { n,base,ptr in
+      var tracker = getSolver(a)
+      ptr[0] = a
+      ptr[1] = b
+      return sequence(tracker,n,base)
+   }
+}
+
+public func |(a : UnsafeMutablePointer<Void>, b : UnsafeMutablePointer<Void>) -> UnsafeMutablePointer<Void> {
+   return packageVoidArray(2) { n,base,ptr in
+      var tracker = getSolver(a)
+      ptr[0] = a
+      ptr[1] = b
+      return alts(tracker,n,base)
+   }
+}
+
+func sum(tracker : ORTracker,R : ORIntRange,b : ORInt -> ORExpr) -> ORExpr {
+   return ORFactory.sum(tracker, over: R, suchThat: nil, of: b)
+}
+func range(tracker : ORTracker,r : Range<Int>) -> ORIntRange {
+   return ORFactory.intRange(tracker, low: ORInt(r.startIndex), up: ORInt(r.endIndex - 1))
 }
 
 let n : ORInt = 8
@@ -40,7 +120,7 @@ cp.search {
       let y = x[k]
       return whileDo(cp,{ !cp.bound(y)}) {
          let v = cp.min(y)
-         return alts(cp,[equal(cp,y,v),diff(cp,y,v)])
+         return equal(cp,y,v) | diff(cp,y,v)
       }
    }
 }
--- Clo/CPSwiftExpe.playground/Contents.swift
+++ Clo/CPSwiftExpe.playground/Contents.swift
@@ -5,44 +5,14 @@ import ORProgram
 
 sizeof(UnsafeMutablePointer<Void>)
 
-var str = "Hello, playground"
-
-func sequence(solver: CPCommonProgram, s: [UnsafeMutablePointer<Void>]) -> UnsafeMutablePointer<Void> {
-   let c : [AnyObject] = unsafeBitCast(s,[AnyObject].self)
-   return sequence(solver,c)
-}
-
-func unwrap<T>(x : UnsafeMutablePointer<Void>) -> T {
-   return unsafeBitCast(x, T.self)
-}
-
 func range(tracker : ORTracker,r : Range<Int>) -> ORIntRange {
    return ORFactory.intRange(tracker, low: ORInt(r.startIndex), up: ORInt(r.endIndex - 1))
 }
 
-infix operator » { associativity left precedence 80 }
-infix operator | { associativity left precedence 80 }
-
-public func »(a : UnsafeMutablePointer<Void>, b : UnsafeMutablePointer<Void>) -> UnsafeMutablePointer<Void> {
-   let at : ORSTask = unwrap(a)
-   let tracker =  at.tracker() as! CPCommonProgram
-   return sequence(tracker, [a,b])
-}
-
-public func |(a : UnsafeMutablePointer<Void>, b : UnsafeMutablePointer<Void>) -> UnsafeMutablePointer<Void> {
-   let at : ORSTask = unwrap(a)
-   let bt : ORSTask = unwrap(a)
-   let tracker =  at.tracker() as! CPCommonProgram
-   return alts(tracker, [at,bt])
-}
+var str = "Hello, playground"
 
 let m  = ORFactory.createModel()
 let cp = ORFactory.createCPProgram(m)
 let x  = ORFactory.intVarArray(m, range: range(m,1..<10), domain: range(m,1..<10))
 
-let e0 = firstFail(cp, x) » firstFail(cp, x)
-let e1 = equal(cp,x[1],1) | diff(cp, x[1],1)
-
-let z0 = unsafeBitCast(e0, UnsafeMutablePointer<ORSTask>.self).memory
-let z1 = unsafeBitCast(e1, UnsafeMutablePointer<ORSTask>.self).memory
 
