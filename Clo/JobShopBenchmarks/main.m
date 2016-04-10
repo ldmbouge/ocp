4c6e0db15c0140aedb4e4ff940c401e1f5bc8811
Laurent Michel <ldm@engr.uconn.edu>
2016-04-10 13:16:45 -0400

    restored working on given bench

--- Clo/JobShopBenchmarks/main.m
+++ Clo/JobShopBenchmarks/main.m
@@ -73,9 +73,9 @@ int main(int argc, const char * argv[]) {
        
         
         NSString* path = [args lastObject];
-        
-        //FILE* data = fopen([path cStringUsingEncoding: NSASCIIStringEncoding], "r");
-        FILE* data = fopen("orb10.jss","r");
+       
+        FILE* data = fopen([path cStringUsingEncoding: NSASCIIStringEncoding], "r");
+        //FILE* data = fopen("orb10.jss","r");
         
         ORInt nbJobs, nbMachines;
         fscanf(data, "%d",&nbJobs);
