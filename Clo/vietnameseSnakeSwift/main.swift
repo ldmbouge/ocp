8f5dc4b360b4a7280abecbf8e59d9cb5422198da
Laurent Michel <ldm@engr.uconn.edu>
2016-06-14 11:23:55 -0400

    fixed snake (swift)

--- Clo/vietnameseSnakeSwift/main.swift
+++ Clo/vietnameseSnakeSwift/main.swift
@@ -15,12 +15,15 @@ autoreleasepool {
    let m  = ORFactory.createModel()
    let x  = ORFactory.intVarArray(m, range: range(m, 1..<10), domain: range(m, 1..<10))
    m.add(ORFactory.alldifferent(x))
-   m.add(x[1] + (x[2]*13)/x[3] + x[4] + x[5]*12 - x[6] - 11 + x[7]*(x[8]/x[9]) - 10 == 66)
+   let t2 = (x[2]*13)/x[3]
+   let t3 = x[7]*(x[8]/x[9])
+   let t4 = (x[5]*12)
+   m.add(x[1] + t2 + x[4] + t4 - x[6] - 11 + t3 - 10 == 66)
    
    var nbs = 0
    let cp = ORFactory.createCPProgram(m)
    
-   cp.search {
+   cp.searchAll {
       firstFail(cp, x) » Do(cp) { nbs = nbs + 1; }
    }
    print("Number of sols \(nbs)")
