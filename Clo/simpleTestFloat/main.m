95f05aa0ca2488ffcbd3a952f37f2c1c95f68344
ZitounCorp <h.zitoun@gmail.com>
2017-02-26 20:04:49 +0100

    Some test

---
+++ Clo/simpleTestFloat/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  simpleTestFloat
+//
+//  Created by Zitoun on 26/09/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ Clo/testGroup/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testGroup
+//
+//  Created by Zitoun on 12/02/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
