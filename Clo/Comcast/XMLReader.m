fbdb7711165b726456b02b694f9223a7b8fad745
Sarah Peck <sarah.peck@uconn.edu>
2017-03-04 21:05:12 -0500

    Update

--- Clo/Comcast/XMLReader.m
+++ Clo/Comcast/XMLReader.m
@@ -7,11 +7,31 @@
 //
 
 #import "XMLReader.h"
+#import "GDataXMLNode.h"
+
+struct cnodes
+{
+    int cnode_id;
+};
 
 @implementation XMLReader
 
 -(id) init {
     self = [super init];
+    
+    
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"sample" ofType:@"xml"];
+    NSData *xmlData = [[NSMutableData alloc] initWithContentsOfFile:filePath];
+    NSError *error;
+    GDataXMLDocument *doc = [[GDataXMLDocument alloc] initWithData:xmlData options:0 error:&error];
+    if (doc == nil) {return nil;}
+    
+    NSLog(@"%@", doc.rootElement);
+    /*
+    [doc release];
+    [xmlData release];
+    */
+    
     return self;
 }
 
