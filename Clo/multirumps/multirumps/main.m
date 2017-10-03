1e30462615e92fc4c764a9ac45ef15b92081816b
Zitoun <zitoun@MacBook-Pro-de-Zitoun-2.local>
2017-10-03 16:00:05 +0200

    fix bug in floatVarArray + adding some benches

---
+++ Clo/multirumps/multirumps/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  multirumps
+//
+//  Created by Zitoun on 03/10/2017.
+//  Copyright © 2017 Zitoun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ Clo/sinetaylorAbs/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sinetaylorAbs
+//
+//  Created by Zitoun on 03/10/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
