465cfd58db2c5d1733ab68a1f1d492cedbdfd6ab
Greg Johnson <greg.johnson@uconn.edu>
2017-11-03 13:34:49 -0400

    Initial version of VSIDS-like heuristic for bit vectors

---
+++ Clo/ORProgram/CPBitVarVSIDS.h
@@ -0,0 +1,29 @@
+/************************************************************************
+ Mozilla Public License
+ 
+ Copyright (c) 2012 NICTA, Laurent Michel and Pascal Van Hentenryck
+ 
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+ ***********************************************************************/
+
+#import <ORProgram/CPBitVarHeuristic.h>
+#import <ORProgram/CPBitVarBaseHeuristic.h>
+#import <ORProgram/CPProgram.h>
+#import <objcp/CPVar.h>
+
+@interface CPBitVarVSIDS : CPBitVarBaseHeuristic<CPBitVarHeuristic> {
+   id<ORVarArray>  _vars;  // Model variables
+   id<ORVarArray>   _cvs;  // concrete variables
+   id<ORVarArray> _rvars;
+   id<CPProgram>     _cp;
+}
+-(CPBitVarVSIDS*)initCPBitVarVSIDS:(id<CPCommonProgram>)cp restricted:(id<ORVarArray>)rvars;
+-(ORFloat)varOrdering:(id<CPBitVar>)x;
+-(ORFloat)valOrdering:(ORUInt)v forVar:(id<CPBitVar>)x ;
+-(void)initInternal:(id<ORVarArray>)t and:(id<CPVarArray>)cv;
+-(id<ORVarArray>)allBitVars;
+-(id<CPProgram>)solver;
+@end
---
+++ Clo/ORProgram/CPBitVarVSIDS.m
@@ -0,0 +1,62 @@
+/************************************************************************
+ Mozilla Public License
+ 
+ Copyright (c) 2012 NICTA, Laurent Michel and Pascal Van Hentenryck
+ 
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+ ***********************************************************************/
+
+#import <ORProgram/CPBitVarVSIDS.h>
+#import <CPUKernel/CPEngine.h>
+#import <objcp/CPBitVar.h>
+
+@implementation CPBitVarVSIDS {
+   id<CPEngine>    _engine;
+}
+-(CPBitVarVSIDS*)initCPBitVarVSIDS:(id<CPProgram>)cp restricted:(id<ORVarArray>)rvars
+{
+   self = [super init];
+   _cp = cp;
+   _engine  = [cp engine];
+   _vars = nil;
+   _rvars = rvars;
+   return self;
+}
+- (id)copyWithZone:(NSZone *)zone
+{
+   CPBitVarVSIDS * cp = [[CPBitVarVSIDS alloc] initCPBitVarVSIDS:_cp restricted:_rvars];
+   return cp;
+}
+-(void)dealloc
+{
+   [super dealloc];
+}
+-(id<CPProgram>)solver
+{
+   return _cp;
+}
+-(id<ORVarArray>)allBitVars
+{
+   return (id<ORVarArray>) (_rvars!=nil ? _rvars : _cvs);
+}
+
+-(ORFloat)varOrdering:(id<CPBitVar>)x
+{
+   float rv = - [x degree];
+   return rv;
+}
+-(ORFloat)valOrdering:(ORUInt)v forVar:(id<CPBitVar>)x
+{
+   return -v;
+}
+-(void)initInternal:(id<ORVarArray>)t and:(id<CPVarArray>)cv
+{
+   _vars = t;
+   _cvs  = cv;
+   NSLog(@"FirstFail ready...");
+}
+
+@end
