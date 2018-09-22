b262216fee090535dcd0ae3e509bac2e8012d9e4
zitoun <h.zitoun@gmail.com>
2018-09-23 00:48:22 +0200

    maxAbsDens*

---
+++ Clo/Float/sine_double/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sine_double
+//
+//  Created by Zitoun on 17/09/2018.
+//  Copyright © 2018 Laurent Michel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+   @autoreleasepool {
+       // insert code here...
+       NSLog(@"Hello, World!");
+   }
+   return 0;
+}
