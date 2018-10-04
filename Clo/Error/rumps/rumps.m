3844ab08b77434d256903b96baf03aa3101118ab
Remy Garcia <remy.garcia@i3s.unice.fr>
2018-10-04 14:36:32 +0200

    Minor fix

--- Clo/Error/rumps/rumps.m
+++ Clo/Error/rumps/rumps.m
@@ -7,21 +7,13 @@
 #import <ORProgram/ORProgram.h>
 #include "gmp.h"
 
-#define printFvar(name, var) NSLog(@""name" : [% 20.20e, % 20.20e]f (%s)",[(id<CPFloatVar>)[cp concretize:var] min],[(id<CPFloatVar>)[cp concretize:var] max],[cp bound:var] ? "YES" : "NO"); NSLog(@"e"name": [% 20.20e, % 20.20e]q",[(id<CPFloatVar>)[cp concretize:var] minErrF],[(id<CPFloatVar>)[cp concretize:var] maxErrF]);
-#define getFmin(var) [(id<CPFloatVar>)[cp concretize:var] min]
-#define getFminErr(var) *[(id<CPFloatVar>)[cp concretize:var] minErr]
-
-#define printDvar(name, var) NSLog(@""name" : [% 24.24e, % 24.24e]d (%s)",[(id<CPDoubleVar>)[cp concretize:var] min],[(id<CPDoubleVar>)[cp concretize:var] max],[cp bound:var] ? "YES" : "NO"); NSLog(@"e"name": [% 24.24e, % 24.24e]q",[(id<CPDoubleVar>)[cp concretize:var] minErrF],[(id<CPDoubleVar>)[cp concretize:var] maxErrF]);
-#define getDmin(var) [(id<CPDoubleVar>)[cp concretize:var] min]
-#define getDminErr(var) *[(id<CPDoubleVar>)[cp concretize:var] minErr]
-
 void check_it_d(double x, double y, double z, id<ORRational> ez) {
     // 333.75 b^6 + a^2 (11 a^2 b^2 - b^6 - 121 b^4 - 2 ) + 5.5 b^8 + a / (2b)
     mpq_t xq, yq, zq, y2q, y4q, y6q, y8q, x2q, tmp0, tmp1;
     double cz = 333.75*y*y*y*y*y*y + x*x*(11.0*x*x*y*y - y*y*y*y*y*y - 121.0*y*y*y*y - 2.0) + 5.5*y*y*y*y*y*y*y*y + x/(2.0*y);
     
     if (cz != z)
-        printf("WRONG: z   = % 24.24e while cz = % 24.24e\n", z, cz);
+        NSLog(@"WRONG: z   = % 24.24e while cz = % 24.24e\n", z, cz);
     
     mpq_inits(xq, yq, zq, y2q, y4q, y6q, y8q, x2q, tmp0, tmp1, NULL);
     mpq_set_d(xq, x);
@@ -100,7 +92,7 @@ void check_it_f(float x, float y, float z, id<ORRational> ez) {
     float cz = 333.75f*y*y*y*y*y*y + x*x*(11.0f*x*x*y*y - y*y*y*y*y*y - 121.0f*y*y*y*y - 2.0f) + 5.5f*y*y*y*y*y*y*y*y + x/(2.0f*y);
     
     if (cz != z)
-        printf("WRONG: z   = % 20.20e while cz = % 20.20e\n", z, cz);
+        NSLog(@"WRONG: z   = % 20.20e while cz = % 20.20e\n", z, cz);
     
     mpq_inits(xq, yq, zq, y2q, y4q, y6q, y8q, x2q, tmp0, tmp1, NULL);
     mpq_set_d(xq, x);
@@ -149,9 +141,8 @@ void rump_f(int search, int argc, const char * argv[]) {
         
         [mdl add:[x_0 set: @(77617.f)]];
         [mdl add:[y_0 set: @(33096.f)]];
-        //[mdl add:[r_0 set: [[[[[[[[[y_0 mul: @(333.75f)] mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0] plus: [[x_0 mul: x_0] mul: [[[[[[[x_0 mul: @(11.0f)] mul: x_0] mul: y_0] mul: y_0] sub: [[[[[y_0 mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0]] sub: [[[[y_0 mul: @(121.0f)] mul: y_0] mul: y_0] mul: y_0]] sub: @(2.0f)]]] plus: [[[[[[[[y_0 mul: @(5.5f)] mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0]] plus: [x_0 div: [y_0 mul: @(2.f)]]]]];
+        [mdl add:[r_0 set: [[[[[[[[[y_0 mul: @(333.75f)] mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0] plus: [[x_0 mul: x_0] mul: [[[[[[[x_0 mul: @(11.0f)] mul: x_0] mul: y_0] mul: y_0] sub: [[[[[y_0 mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0]] sub: [[[[y_0 mul: @(121.0f)] mul: y_0] mul: y_0] mul: y_0]] sub: @(2.0f)]]] plus: [[[[[[[[y_0 mul: @(5.5f)] mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0] mul: y_0]] plus: [x_0 div: [y_0 mul: @(2.f)]]]]];
         //assert((r_0 >= 0));
-        [mdl add:[r_0 set:[x_0 plus: y_0]]];
         //[mdl add:[r_0 geq:@(0.0f)]];
         //[model add:[[r_0 lt:@(0.0f)] lor:[r_0 gt:@(0.0f)]]];
         
@@ -175,10 +166,7 @@ void rump_f(int search, int argc, const char * argv[]) {
 }
 
 int main(int argc, const char * argv[]) {
-
-    rump_f(1, argc, argv);
-    //rump_d(1, argc, argv);
-    NSLog(@"%16.16e", FLT_MAX);
+    //rump_f(1, argc, argv);
+    rump_d(1, argc, argv);
     return 0;
 }
-
