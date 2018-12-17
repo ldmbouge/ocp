e62d60ff6429aa03067787da95af7e2f7d1b646a
zitoun <h.zitoun@gmail.com>
2018-12-17 22:28:58 +0100

    add new benches

---
+++ Clo/Float/div1_10/main.m
@@ -0,0 +1,52 @@
+#import <ORProgram/ORProgram.h>
+#import "ORCmdLineArgs.h"
+
+#define LARGE_NUMBER 4.0e+14
+#define NBLOOPS 10
+
+
+int main(int argc, const char * argv[]) {
+   @autoreleasepool {
+      ORCmdLineArgs* args = [ORCmdLineArgs newWith:argc argv:argv];
+      [args measure:^struct ORResult(){
+         
+         id<ORModel> model = [ORFactory createModel];
+         
+         id<ORFloatVarArray> d = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"d"];
+         id<ORFloatVarArray> nextValue = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS-1) names:@"nextValue"];
+         
+         id<ORGroup> g = [args makeGroup:model];
+         
+         [g add:[d[0] eq:@(LARGE_NUMBER)]];
+         
+         for(ORInt i = 0; i < NBLOOPS; i++){
+            [g add:[nextValue[i] lt:@(2.14748e+09f)]];
+            [g add:[nextValue[i] geq:@(1.f)]];
+            [g add:[d[i+1] eq:[d[i] div:nextValue[i]]]];
+         }
+         
+         [g add:[d[NBLOOPS] lt:@(1.f)]];
+         
+         [model add:g];
+         //         NSLog(@"%@",model);
+         id<ORFloatVarArray> vars = [model floatVars];
+         id<CPProgram> cp = [args makeProgram:model];
+         __block bool found = false;
+         [cp solveOn:^(id<CPCommonProgram> p) {
+            [args launchHeuristic:((id<CPProgram>)p) restricted:vars];
+            found = true;
+            for(id<ORFloatVar> v in vars){
+               found &= [p bound: v];
+               NSLog(@"%@ : %16.16e (%s)",v,[p floatValue:v],[p bound:v] ? "YES" : "NO");
+            }
+         } withTimeLimit:[args timeOut]];
+         NSLog(@"nb fail : %d",[[cp engine] nbFailures]);
+         struct ORResult r = REPORT(found, [[cp explorer] nbFailures],[[cp explorer] nbChoices], [[cp engine] nbPropagation]);
+         return r;
+      }];
+      
+   }
+   return 0;
+}
+
+
---
+++ Clo/Float/div3_40/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  div3_40
+//
+//  Created by Zitoun on 17/12/2018.
+//  Copyright © 2018 Laurent Michel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+   @autoreleasepool {
+       // insert code here...
+       NSLog(@"Hello, World!");
+   }
+   return 0;
+}
