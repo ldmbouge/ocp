0af5d8a688ccf8d2955fdee5578d86b2729fde2b
zitounh <h.zitoun@gmail.com>
2019-08-27 11:20:55 -0400

    fix rocket for cpjm

--- Clo/Float/Rocket_diff_10/main.m
+++ Clo/Float/Rocket_diff_10/main.m
@@ -112,12 +112,6 @@
 #define NBLOOPS 1
 
 int main(int argc, const char * argv[]) {
-   /*
-    1: Logic
-    2: Guarded
-    */
-   int first_if = 2;
-   
    @autoreleasepool {
       ORCmdLineArgs* args = [ORCmdLineArgs newWith:argc argv:argv];
       
@@ -125,25 +119,29 @@ int main(int argc, const char * argv[]) {
       
       NSMutableArray* toadd = [[NSMutableArray alloc] init];
       
-      // Constants
-      id<ORExpr> Mf   = [ORFactory float:model value:150000.f];
-      id<ORExpr> R = [ORFactory float:model value:6.4e6f];
-      id<ORExpr> A  = [ORFactory float:model value:140.0f];
-      id<ORExpr> G  = [ORFactory float:model value:6.67428e-11f];
-      id<ORExpr> M_t  = [ORFactory float:model value:5.9736e24f];
-      id<ORExpr> D  = [ORFactory float:model value:([D floatValue] + 4.0e5f)];
-      id<ORExpr> v_l  = [ORFactory float:model value:(0.7f * sqrtf(([G floatValue] * [M_t floatValue]) / [D floatValue]))];
+      // Input variable
+      id<ORFloatVar> Mf = [ORFactory floatVar:model low:1000.0f up:1e6f name:@"Mf"]; // Example: 150000.f
+      id<ORFloatVar> A  = [ORFactory floatVar:model low:5.0f up:200.0f name:@"A"];  // Example: 140.0f
       
-//      other constants
-       id<ORExpr> c1   = [ORFactory float:model value:-2.0f];
-       id<ORExpr> c2   = [ORFactory float:model value:0.166666667f];
-       id<ORExpr> c3   = [ORFactory float:model value:0.66743e-10f];
-       id<ORExpr> c4   = [ORFactory float:model value:59735.99e20f];
-   
+      // Constants
+      id<ORExpr> dt   = [ORFactory float:model value:0.1f];
+      //id<ORExpr> R = [ORFactory float:model value:6.4e6f];
+      id<ORExpr> G  = [ORFactory float:model value:6.67428e-11f];
+      id<ORExpr> Mt  = [ORFactory float:model value:5.9736e24f];
+      //id<ORExpr> D  = [ORFactory float:model value:([R floatValue] + 4.0e5f)];
+      //id<ORExpr> v_l  = [ORFactory float:model value:(0.7f * sqrtf(([G floatValue] * [Mt floatValue]) / [D floatValue]))];
+      
+      
       
       // Local vars
       //      --------
       // original var
+      id<ORFloatVarArray> c = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"c"];
+      id<ORFloatVarArray> s = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"s"];
+      id<ORFloatVarArray> x = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"x"];
+      id<ORFloatVarArray> y = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"y"];
+      id<ORFloatVarArray> t = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"t"];
+      
       id<ORFloatVarArray> u1 = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"u1"];
       id<ORFloatVarArray> u2 = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"u2"];
       id<ORFloatVarArray> u3 = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"u3"];
@@ -172,6 +170,12 @@ int main(int argc, const char * argv[]) {
       //      --------
       //      --------
       // optimized var
+      id<ORFloatVarArray> c_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"c_opt"];
+      id<ORFloatVarArray> s_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"s_opt"];
+      id<ORFloatVarArray> x_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"x_opt"];
+      id<ORFloatVarArray> y_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"y_opt"];
+      id<ORFloatVarArray> t_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"t_opt"];
+      
       id<ORFloatVarArray> u1_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"u1_opt"];
       id<ORFloatVarArray> u2_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"u2_opt"];
       id<ORFloatVarArray> u3_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"u3_opt"];
@@ -195,7 +199,6 @@ int main(int argc, const char * argv[]) {
       id<ORFloatVarArray> w2_3_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"w2_3_opt"];
       id<ORFloatVarArray> w2_4_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"w2_4_opt"];
       
-      
       id<ORFloatVarArray> m2_f_opt = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"m2_f_opt"];
       
       id<ORFloatVarArray> TMP_2 = [ORFactory floatVarArray:model range:RANGE(model, 0, NBLOOPS) names:@"TMP_2"];
@@ -216,6 +219,7 @@ int main(int argc, const char * argv[]) {
       [toadd addObject:[w3[0] set:@(0.0f)]];
       [toadd addObject:[w4[0] set:@(0.0f)]];
       [toadd addObject:[mf[0] set:@(0.0f)]];
+      [toadd addObject:[t[0] set:@(0.0f)]];
       
       [toadd addObject:[u1_opt[0] set:@(0.0f)]];
       [toadd addObject:[u2_opt[0] set:@(0.0f)]];
@@ -226,17 +230,117 @@ int main(int argc, const char * argv[]) {
       [toadd addObject:[w3_opt[0] set:@(0.0f)]];
       [toadd addObject:[w4_opt[0] set:@(0.0f)]];
       [toadd addObject:[mf_opt[0] set:@(0.0f)]];
+      [toadd addObject:[t_opt[0] set:@(0.0f)]];
       
       for (ORUInt n = 1; n <= NBLOOPS; n++) {
+         { // Guarded
+            id<ORExpr> ifCond = [mf[n-1] gt: @(0.0f)];
+            id<ORIntVar> thenGuard = [ORFactory boolVar:model];
+            id<ORIntVar> elseGuard = [ORFactory boolVar:model];
+            [toadd addObject: [ifCond eq: thenGuard]];
+            [toadd addObject: [[ifCond neg] eq: elseGuard]];
+            
+            id<ORGroup> thenGroup = [ORFactory group:model guard:thenGuard];
+            [thenGroup add: [u2_1[n] set: [[u2[n-1] mul: dt] plus: u1[n-1]]]];
+            [thenGroup add: [u2_3[n] set: [[u4[n-1] mul: dt] plus: u3[n-1]]]];
+            [thenGroup add: [w2_1[n] set: [[w2[n-1] mul: dt] plus: w1[n-1]]]];
+            [thenGroup add: [w2_3[n] set: [[w4[n-1] mul: dt] plus: w3[n-1]]]];
+            [thenGroup add: [u2_2[n] set: [[[[[[G minus] mul: Mt] div: [u1[n-1] mul: u1[n-1]]] mul: dt] plus: [[[u1[n-1] mul: u4[n-1]] mul: u4[n-1]] mul: dt]] plus: u2[n-1]]]];
+            [thenGroup add: [u2_4[n] set: [[[[[@(-2.0f) mul: u2[n-1]] mul: u4[n-1]] div: u1[n-1]] mul: dt] plus: u4[n-1]]]];
+            [thenGroup add: [w2_2[n] set: [[[[[[[G minus] mul: Mt] div: [w1[n-1] mul: w1[n-1]]] mul: dt] plus: [[[w1[n-1] mul: w4[n-1]] mul: w4[n-1]] mul: dt]] plus: [[[A mul: w2[n-1]] div: [Mf sub: [A mul: t[n-1]]]] mul: dt]] plus: w2[n-1]]]];
+            [thenGroup add: [w2_4[n] set: [[[[[[@(-2.0f) mul: w2[n-1]] mul: w4[n-1]] div: w1[n-1]] mul: dt] plus: [[[A mul: w4[n-1]] div: [Mf sub: [A mul: t[n-1]]]] mul: dt]] plus: w4[n-1]]]];
+            [thenGroup add: [m2_f[n] set: [mf[n-1] sub: [A mul: t[n-1]]]]];
+            [thenGroup add: [t[n] set: [t[n-1] plus: dt]]];
+            [toadd addObject:thenGroup];
+            
+            id<ORGroup> elseGroup = [ORFactory group:model guard:elseGuard];
+            [elseGroup add: [u2_1[n] set: [[u2[n-1] mul: dt] plus: u1[n-1]]]];
+            [elseGroup add: [u2_3[n] set: [[u4[n-1] mul: dt] plus: u3[n-1]]]];
+            [elseGroup add: [w2_1[n] set: [[w2[n-1] mul: dt] plus: w1[n-1]]]];
+            [elseGroup add: [w2_3[n] set: [[w4[n-1] mul: dt] plus: w3[n-1]]]];
+            [elseGroup add: [u2_2[n] set: [[[[[[G minus] mul: Mt] div: [u1[n-1] mul: u1[n-1]]] mul: dt] plus: [[[u1[n-1] mul: u4[n-1]] mul: u4[n-1]] mul: dt]] plus: u2[n-1]]]];
+            [elseGroup add: [u2_4[n] set: [[[[[@(-2.0f) mul: u2[n-1]] mul: u4[n-1]] div: u1[n-1]] mul: dt] plus: u4[n-1]]]];
+            [elseGroup add: [w2_2[n] set: [[[[[[G minus] mul: Mt] div: [w1[n-1] mul: w1[n-1]]] mul: dt] plus: [[[w1[n-1] mul: w4[n-1]] mul: w4[n-1]] mul: dt]] plus: w2[n-1]]]];
+            [elseGroup add: [w2_4[n] set: [[[[[@(-2.0f) mul: w2[n-1]] mul: w4[n-1]] div: w1[n-1]] mul: dt] plus: w4[n-1]]]];
+            [elseGroup add: [m2_f[n] set: mf[n-1]]];
+            [elseGroup add: [t[n] set: [t[n-1] plus: dt]]];
+            [toadd addObject:elseGroup];
+         }
+         
+         [toadd addObject:[c[n] set: [@(1.0f) sub: [[w2_3[n] mul: u2_3[n]] mul: @(0.5f)]]]] ;
+         [toadd addObject:[s[n] set: [u2_3[n] sub: [[[u2_3[n] mul: u2_3[n]] mul: u2_3[n]] div: @(0.166666667f)]]]];
+         [toadd addObject:[x[n] set: [w2_1[n] mul: c[n]]]];
+         [toadd addObject:[y[n] set: [w2_1[n] mul: s[n]]]];
+         [toadd addObject:[u1[n] set: u2_1[n]]];
+         [toadd addObject:[u2[n] set: u2_2[n]]];
+         [toadd addObject:[u3[n] set: u2_3[n]]];
+         [toadd addObject:[u4[n] set: u2_4[n]]];
+         [toadd addObject:[w1[n] set: w2_1[n]]];
+         [toadd addObject:[w2[n] set: w2_2[n]]];
+         [toadd addObject:[w3[n] set: w2_3[n]]];
+         [toadd addObject:[w4[n] set: w2_4[n]]];
+         [toadd addObject:[mf[n] set: m2_f[n]]];
+         
+         { // Guarded
+            id<ORExpr> ifCond = [mf_opt[n-1] gt: @(0.0f)];
+            id<ORIntVar> thenGuard = [ORFactory boolVar:model];
+            id<ORIntVar> elseGuard = [ORFactory boolVar:model];
+            [toadd addObject: [ifCond eq: thenGuard]];
+            [toadd addObject: [[ifCond neg] eq: elseGuard]];
+            
+            id<ORGroup> thenGroup = [ORFactory group:model guard:thenGuard];
+            [thenGroup add: [TMP_2[n] set: [u1_opt[n-1] mul: u1_opt[n-1]]]];
+            [thenGroup add: [TMP_4[n] set: [@(59735.99e20f) div: [w1_opt[n-1] mul: w1_opt[n-1]]]]];
+            [thenGroup add: [TMP_10[n] set: [@(140.0f) mul: t_opt[n-1]]]];
+            [thenGroup add: [m2_f_opt[n] set: [mf_opt[n-1] plus: [t_opt[n-1] mul: @(-140.0f)]]]];
+            [thenGroup add: [u2_1_opt[n] set: [u1_opt[n-1] plus: [u2_opt[n-1] mul: @(0.1f)]]]];
+            [thenGroup add: [u2_3_opt[n] set: [u3_opt[n-1] plus: [u4_opt[n-1] mul: @(0.1f)]]]];
+            [thenGroup add: [w2_1_opt[n] set: [w1_opt[n-1] plus: [w2_opt[n-1] mul: @(0.1f)]]]];
+            [thenGroup add: [w2_3_opt[n] set: [w3_opt[n-1] plus: [w4_opt[n-1] mul: @(0.1f)]]]];
+            [thenGroup add: [u2_2_opt[n] set: [[[[[@(0.66743e-10f) mul: [@(59735.99e20f) div: TMP_2[n]]] mul: @(0.1f)] minus] plus: [[[u1_opt[n-1] mul: u4_opt[n-1]] mul: u4_opt[n-1]] mul: @(0.1f)]] plus: u2_opt[n-1]]]];
+            [thenGroup add: [u2_4_opt[n] set: [[[@(-2.0f) mul: [u2_opt[n-1] mul: [u4_opt[n-1] div: u1_opt[n-1]]]] mul: @(0.1f)] plus: u4_opt[n-1]]]];
+            [thenGroup add: [w2_2_opt[n] set: [[[[[[@(0.66743e-10f) mul: TMP_4[n]] mul: @(0.1f)] minus] plus: [[[w1_opt[n-1] mul: w4_opt[n-1]] mul: w4_opt[n-1]] mul: @(0.1f)]] plus: [[[@(140.0f) mul: w2_opt[n-1]] div: [@(150000.0f) sub: [@(140.0f) mul: t_opt[n-1]]]] mul: @(0.1f)]] plus: w2_opt[n-1]]]];
+            [thenGroup add: [w2_4_opt[n] set: [[[[@(-2.0f) mul: [w2_opt[n-1] mul: [w4_opt[n-1] div: w1_opt[n-1]]]] mul: @(0.1f)] plus: [[@(140.0f) mul: [w4_opt[n-1] div: [@(150000.0f) sub: TMP_10[n]]]] mul: @(0.1f)]] plus: w4_opt[n-1]]]];
+            [thenGroup add: [t_opt[n] set: [t_opt[n-1] plus: @(0.1f)]]];
+            [toadd addObject:thenGroup];
+            
+            id<ORGroup> elseGroup = [ORFactory group:model guard:elseGuard];
+            [elseGroup add: [TMP_2[n] set: [u1_opt[n-1] mul: u1_opt[n-1]]]];
+            [elseGroup add: [TMP_14[n] set: [w1_opt[n-1] mul: w1_opt[n-1]]]];
+            [elseGroup add: [u2_1_opt[n] set: [u1_opt[n-1] plus: [u2_opt[n-1] mul: @(0.1f)]]]];
+            [elseGroup add: [u2_3_opt[n] set: [u3_opt[n-1] plus: [u4_opt[n-1] mul: @(0.1f)]]]];
+            [elseGroup add: [w2_1_opt[n] set: [w1_opt[n-1] plus: [w2_opt[n-1] mul: @(0.1f)]]]];
+            [elseGroup add: [w2_3_opt[n] set: [w3_opt[n-1] plus: [w4_opt[n-1] mul: @(0.1f)]]]];
+            [elseGroup add: [u2_2_opt[n] set: [[[[[@(0.66743e-10f) mul: [@(59735.99e20f) div: TMP_2[n]]] mul: @(0.1f)] minus] plus: [[[u1_opt[n-1] mul: u4_opt[n-1]] mul: u4_opt[n-1]] mul: @(0.1f)]] plus: u2_opt[n-1]]]] ;
+            [elseGroup add: [u2_4_opt[n] set: [[[@(-2.0f) mul: [u2_opt[n-1] mul: [u4_opt[n-1] div: u1_opt[n-1]]]] mul: @(0.1f)] plus: u4_opt[n-1]]]];
+            [elseGroup add: [w2_2_opt[n] set: [[[[[@(0.66743e-10f) mul: [@(59735.99e20f) div: TMP_14[n]]] mul: @(0.1f)] minus] plus: [[[w1_opt[n-1] mul: w4_opt[n-1]] mul: w4_opt[n-1]] mul: @(0.1f)]] plus: w2_opt[n-1]]]];
+            [elseGroup add: [w2_4_opt[n] set: [[[@(-2.0f) mul: [w2_opt[n-1] mul: [w4_opt[n-1] div: w1_opt[n-1]]]] mul: @(0.1f)] plus: w4_opt[n-1]]]] ;
+            [elseGroup add: [t_opt[n] set: [t_opt[n-1] plus: @(0.1f)]]];
+            [toadd addObject:elseGroup];
+         }
+         
+         [toadd addObject:[c_opt[n] set: [@(1.0f) sub: [[u2_3_opt[n] plus: [u2_4_opt[n] mul: @(0.1f)]] mul: [@(0.5f) mul: [u2_3_opt[n] plus: [u2_4_opt[n] mul: @(0.1f)]]]]]]];
+         [toadd addObject:[s_opt[n] set: [[u2_3_opt[n] plus: [u2_4_opt[n] mul: @(0.1f)]] sub: [[[u2_3_opt[n] plus: [u2_4_opt[n] mul: @(0.1f)]] mul: [[u2_3_opt[n] plus: [u2_4_opt[n] mul: @(0.1f)]] mul: [u2_3_opt[n] plus: [u2_4_opt[n] mul: @(0.1f)]]]] div: @(0.166666667f)]]]];
+         [toadd addObject:[x_opt[n] set: [c_opt[n] mul: [u2_1_opt[n] plus: [u2_2_opt[n] mul: @(0.1f)]]]]] ;
+         [toadd addObject:[y_opt[n] set: [s_opt[n] mul: [u2_1_opt[n] plus: [u2_2_opt[n] mul: @(0.1f)]]]]] ;
+         [toadd addObject:[u1_opt[n] set: u2_1_opt[n]]];
+         [toadd addObject:[u2_opt[n] set: u2_2_opt[n]]];
+         [toadd addObject:[u3_opt[n] set: u2_3_opt[n]]];
+         [toadd addObject:[u4_opt[n] set: u2_4_opt[n]]];
+         [toadd addObject:[w1_opt[n] set: w2_1_opt[n]]];
+         [toadd addObject:[w2_opt[n] set: w2_2_opt[n]]];
+         [toadd addObject:[w3_opt[n] set: w2_3_opt[n]]];
+         [toadd addObject:[w4_opt[n] set: w2_4_opt[n]]];
+         [toadd addObject:[mf_opt[n] set: m2_f_opt[n]]] ;
          
       }
       
-      [toadd addObject:[diff set: [mf_opt[NBLOOPS] sub: mf[NBLOOPS]]]];
+      [toadd addObject:[diff set: [x_opt[NBLOOPS] sub: x[NBLOOPS]]]];
       
-      [toadd addObject:[[diff mul: diff] geq:@(5.96e-8f)]];
+      //[toadd addObject:[[diff mul: diff] geq:@(5.96e-8f)]];
       
       id<CPProgram> cp = [args makeProgramWithSimplification:model constraints:toadd];
-      [ORCmdLineArgs defaultRunner:args model:model program:cp restricted:@[u]];
+      [ORCmdLineArgs defaultRunner:args model:model program:cp restricted:@[Mf,A]];
       
    }
    return 0;
---
+++ Clo/Float/Runge_Kutta_4_10/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Runge_Kutta_4_10
+//
+//  Created by zitoun on 8/27/19.
+//  Copyright © 2019 Laurent Michel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+   @autoreleasepool {
+       // insert code here...
+       NSLog(@"Hello, World!");
+   }
+   return 0;
+}
