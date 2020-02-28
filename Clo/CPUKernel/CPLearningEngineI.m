b63e0fa8c20ec1bbca2bf8fc631985a8bc5fd078
Remy Garcia <remy.garcia@i3s.unice.fr>
2020-02-28 16:21:59 +0100

    import caused cycle dependency

--- Clo/CPUKernel/CPLearningEngineI.m
+++ Clo/CPUKernel/CPLearningEngineI.m
@@ -10,7 +10,6 @@
  ***********************************************************************/
 
 #import "CPLearningEngineI.h"
-#import "CPBitVar.h"
 
 @implementation CPLearningEngineI
 -(CPLearningEngineI*) initEngine: (id<ORTrail>) trail memory:(id<ORMemoryTrail>)mt tracer:(id<ORTracer>)tr
